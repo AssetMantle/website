308111ff562a465cd43420c19e9227ccc5aa21bd
blackpanther1881 <raju@persistence.one>
2021-10-29 18:49:56 +0530

    updating links

--- src/views/Hompage.js
+++ src/views/Hompage.js
@@ -42,7 +42,7 @@ const HomePage = () => {
                             <p className="banner-content">Be the first to receive Asset Mantle updates, latest projects,announcements and sneak peeks.
                             </p>
                             <div className="buttons-group">
-                            <Button variant="secondary" className="button-double-border">Join Whitelist(bob123@gmail.com)</Button>
+                            <Button variant="secondary" className="button-double-border" disabled>Join Whitelist(bob123@gmail.com)</Button>
                             </div>
                         </div>
                     </div>
