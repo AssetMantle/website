323f90c6ad4f1123dd4be1b2e75038ad007f2ddd
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-02-14 21:12:25 +0600

    🛠footer completed

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,76 +1,91 @@
-import React from 'react';
-import {OverlayTrigger, Tooltip} from "react-bootstrap";
-import Icon from "./icons";
+import React from "react";
+import { FooterContainer } from "../styles/layout/FooterStyle";
+import DATA from "../data/footerData.json";
+import { useState } from "react";
+
 const Footer = () => {
-    const socialList = [
-        {
-            url: "https://twitter.com/AssetMantle",
-            iconName: 'twitter-logo',
-            tooltip: 'twitter'
-        },{
-            url: "https://t.me/assetmantle",
-            iconName: 'telegram-plane',
-            tooltip: 'telegram'
-        }, {
-            url: "https://discord.com/invite/EJr6uwJCjw",
-            iconName: 'discord',
-            tooltip: 'discord'
-        },
-    ];
-    return (
-       
-        <div className="footer">
-            <div className="container">
-                <div className="bottom-section">
-                    <div>
-                       <div className="blogs">
-                           <a href="https://persistence.one" target="_blank" rel="noopener noreferrer" className="help">
-                               Company
-                           </a>
-                           <a href="https://notes.persistence.one/s/Jufq_ChF-" target="_blank" rel="noopener noreferrer" className="blog">
-                               Docs
-                           </a>
-                           <a href="https://blog.assetmantle.one" target="_blank" rel="noopener noreferrer" className="blog">
-                               Blog
-                           </a>
-                       </div>
+  const [email, setEmail] = useState();
+  console.log(email);
 
-                    </div>
-                    <div className="top-section">
-                        <div className="logo">
-                            <img src='/images/logo.svg' alt="logo"/>
-                        </div>
-                        <p className="footer-text">© {new Date().getFullYear()} | Built with <Icon viewClass="social_icon_imgg"
-                                                                          icon="footer_heart" /> by Persistence.</p>
-                    </div>
-                    <div className="bottom-right-section">
-                        <div className="social-links-section">
-                            <ul className="list-unstyled footer-list">
-                                {
-                                    socialList.map((item,) => (
-                                        <OverlayTrigger
-                                            key={item.iconName}
-                                            placement="top"
-                                            overlay={
-                                                <Tooltip id={`tooltip-${item.iconName}}`}>
-                                                    {item.tooltip}
-                                                </Tooltip>
-                                            }
-                                        >
-                                            <a href={item.url}  rel="noopener noreferrer"
-                                                target="_blank"><Icon viewClass="social_icon_imgg"
-                                                    icon={item.iconName} /></a>
-                                        </OverlayTrigger>
-                                    ))
-                                }
-
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <FooterContainer>
+      <h2 className="footer__title">{DATA.h2}</h2>
+      <p className="footer__details">{DATA.p_l}</p>
+      <div className="footer_container">
+        <div className="footer_container__element">
+          <h3>{DATA.left.h3}</h3>
+          <p className="footer_container__element_label">{DATA.left.p_s}</p>
+          <form className="footer_container__element_form">
+            <input
+              type="email"
+              placeholder={DATA.left.placeHolder}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit">Join</button>
+          </form>
         </div>
-    );
+        <div className="footer_container__element">
+          <h3>{DATA.right.h3}</h3>
+          <div className="footer_container__element_media__icons">
+            {React.Children.toArray(
+              DATA.right.links.map((data) => (
+                <a
+                  className="footer_container__element_media__icons_icon"
+                  href={data.href}
+                >
+                  <img src={`/images/${data.icon}.png`} alt={data.alt} />
+                </a>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="footer_container">
+        <div className="footer_container__element">
+          <p>
+            {DATA.left.link_1.text}
+            <a
+              href={DATA.left.link_1.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {DATA.left.link_1.btnTr}
+            </a>
+          </p>
+          <p>
+            {DATA.left.link_2.text}
+            <a
+              href={DATA.left.link_2.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {DATA.left.link_2.btnTr}
+            </a>
+          </p>
+        </div>
+        <div className="footer_container__element">
+          <p>
+            <a
+              href={DATA.right.link1.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {DATA.right.link1.text}
+            </a>
+          </p>
+          <p>
+            <a
+              href={DATA.right.link2.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {DATA.right.link2.text}
+            </a>
+          </p>
+        </div>
+      </div>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
