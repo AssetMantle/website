8faf7384d41bb126e28dc41fe050dfdbfda6bb7b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-02-21 18:29:39 +0600

    🔖testimonial added

---
+++ src/pages/Home/Testimonial.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+const Testimonial = () => {
+  const DaTa = [
+    {
+      pm6: "Lorem ipsum dolor",
+      pm: "“Lorem ipsum dolor sit amet, consectetur adipiscin elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”",
+      img: "Mask Group",
+      namePl6: "User long name",
+      psd: "Dolor sit amet",
+    },
+    {
+      pm6: "Lorem ipsum dolor",
+      pm: "“Lorem ipsum dolor sit amet, consectetur adipiscin elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”",
+      img: "Mask Group",
+      namePl6: "User long name",
+      psd: "Dolor sit amet",
+    },
+  ];
+
+  return (
+    <section className="section_testimonial">
+      <h2>Testimonials</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className="section_testimonial_slick">
+        <Splide
+          options={{
+            type: "loop",
+            pagination: false,
+            perPage: 2,
+            perMove: 1,
+            speed: 400,
+            interval: 3,
+            gap: "24px",
+            breakpoints: {
+              548: {
+                perPage: 1,
+              },
+            },
+          }}
+        >
+          {React.Children.toArray(
+            DaTa.map((data) => (
+              <SplideSlide>
+                <div className="section_testimonial_slick__option">
+                  <p className="testimonial_title">{data.pm6}</p>
+                  <p className="testimonial_details">{data.pm}</p>
+                  <div className="testimonial_owner">
+                    <div className="testimonial_owner__element">
+                      <img src={`/images/landing/prof_img.png`} alt="" />
+                    </div>
+                    <div className="testimonial_owner__element">
+                      <p className="testimonial_owner__element_name">
+                        {data.namePl6}
+                      </p>
+                      <p className="testimonial_owner__element_details">
+                        {data.psd}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SplideSlide>
+            ))
+          )}
+        </Splide>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonial;
