d9f02640f274e2066bb1c00eea6105cc5ea8baac
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-02-24 19:19:20 +0600

    🔖🛠box added

--- src/pages/Home/Stores.js
+++ src/pages/Home/Stores.js
@@ -2,8 +2,143 @@ import React from "react";
 
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { StoreBox } from "../../components/StoreBox";
 
 const Stores = () => {
+  const featuredStore = [
+    {
+      viewerImage: "/images/landing/store/1.png",
+      imageGrid: [
+        "/images/landing/store/2.png",
+        "/images/landing/store/3.png",
+        "/images/landing/store/4.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+    {
+      viewerImage: "/images/landing/store/5.png",
+      imageGrid: [
+        "/images/landing/store/6.png",
+        "/images/landing/store/7.png",
+        "/images/landing/store/8.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+    {
+      viewerImage: "/images/landing/store/9.png",
+      imageGrid: [
+        "/images/landing/store/10.png",
+        "/images/landing/store/11.png",
+        "/images/landing/store/12.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+  ];
+
+  const newStore = [
+    {
+      viewerImage: "/images/landing/store/13.png",
+      imageGrid: [
+        "/images/landing/store/14.png",
+        "/images/landing/store/3.png",
+        "/images/landing/store/16.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+    {
+      viewerImage: "/images/landing/store/17.png",
+      imageGrid: [
+        "/images/landing/store/6.png",
+        "/images/landing/store/7.png",
+        "/images/landing/store/8.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+    {
+      viewerImage: "/images/landing/store/21.png",
+      imageGrid: [
+        "/images/landing/store/22.png",
+        "/images/landing/store/23.png",
+        "/images/landing/store/12.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+  ];
+
+  const popularStore = [
+    {
+      viewerImage: "/images/landing/store/25.png",
+      imageGrid: [
+        "/images/landing/store/2.png",
+        "/images/landing/store/3.png",
+        "/images/landing/store/4.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+    {
+      viewerImage: "/images/landing/store/29.png",
+      imageGrid: [
+        "/images/landing/store/6.png",
+        "/images/landing/store/7.png",
+        // "/images/landing/store/8.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+    {
+      viewerImage: "/images/landing/store/33.png",
+      imageGrid: [
+        "/images/landing/store/10.png",
+        "/images/landing/store/11.png",
+        "/images/landing/store/36.png",
+      ],
+      profileImage: "/images/landing/store/profile.png",
+      name: "Lorem ipsum dolor sit",
+      description:
+        "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    },
+  ];
+
+  const sliderOption = {
+    type: "slide",
+    pagination: false,
+    perPage: 3,
+    perMove: 1,
+    speed: 400,
+    interval: 3,
+    breakpoints: {
+      1330: {
+        perPage: 2,
+      },
+      948: {
+        perPage: 1,
+      },
+    },
+  };
+
   return (
     <section className="section_stores">
       <h2>Our Stores</h2>
@@ -11,270 +146,82 @@ const Stores = () => {
         Discover an awesome range of NFTs from games, art, collectibles, and
         more. Get ready to raise those digital envy meters!
       </p>
-      <h3>Featured Stores</h3>
+      <h3 className="section_stores__title">
+        Featured Stores{" "}
+        <span
+          className="section_stores__title__link_span"
+          id="featured_stores"
+        ></span>
+      </h3>
       <div className="section_stores__element">
-        <Splide
-          options={{
-            type: "slide",
-            pagination: false,
-            perPage: 3,
-            perMove: 1,
-            speed: 400,
-            interval: 3,
-            breakpoints: {
-              1330: {
-                perPage: 2,
-              },
-              948: {
-                perPage: 1,
-              },
-            },
-          }}
-        >
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/1.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/2.png" alt="" />
-                <img src="/images/landing/store/3.png" alt="" />
-                <img src="/images/landing/store/4.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <div className="section_stores__element_card__profile_image">
-                  <img src="/images/landing/store/profile.png" alt="" />
-                </div>
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/5.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/6.png" alt="" />
-                <img src="/images/landing/store/7.png" alt="" />
-                <img src="/images/landing/store/8.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/9.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/10.png" alt="" />
-                <img src="/images/landing/store/11.png" alt="" />
-                <img src="/images/landing/store/12.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
+        <Splide options={sliderOption}>
+          {React.Children.toArray(
+            featuredStore.map((data) => (
+              <SplideSlide>
+                <StoreBox
+                  viewerImage={data.viewerImage}
+                  gridImage1={data.imageGrid[0]}
+                  gridImage2={data.imageGrid[1]}
+                  gridImage3={data.imageGrid[2]}
+                  profileImage={data.profileImage}
+                  name={data.name}
+                  description={data.description}
+                />
+              </SplideSlide>
+            ))
+          )}
         </Splide>
       </div>
-      <h3>New Stores</h3>
+      <h3 className="section_stores__title">
+        New Stores{" "}
+        <span
+          className="section_stores__title__link_span"
+          id="new_stores"
+        ></span>
+      </h3>
       <div className="section_stores__element">
-        <Splide
-          options={{
-            type: "slide",
-            pagination: false,
-            perPage: 3,
-            perMove: 1,
-            speed: 400,
-            interval: 3,
-            breakpoints: {
-              1330: {
-                perPage: 2,
-              },
-              948: {
-                perPage: 1,
-              },
-            },
-          }}
-        >
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/5.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/6.png" alt="" />
-                <img src="/images/landing/store/7.png" alt="" />
-                <img src="/images/landing/store/8.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/9.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/10.png" alt="" />
-                <img src="/images/landing/store/11.png" alt="" />
-                <img src="/images/landing/store/12.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/1.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/2.png" alt="" />
-                <img src="/images/landing/store/3.png" alt="" />
-                <img src="/images/landing/store/4.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
+        <Splide options={sliderOption}>
+          {React.Children.toArray(
+            newStore.map((data) => (
+              <SplideSlide>
+                <StoreBox
+                  viewerImage={data.viewerImage}
+                  gridImage1={data.imageGrid[0]}
+                  gridImage2={data.imageGrid[1]}
+                  gridImage3={data.imageGrid[2]}
+                  profileImage={data.profileImage}
+                  name={data.name}
+                  description={data.description}
+                />
+              </SplideSlide>
+            ))
+          )}
         </Splide>
       </div>
-      <h3>Popular Stores</h3>
+      <h3 className="section_stores__title">
+        Popular Stores{" "}
+        <span
+          className="section_stores__title__link_span"
+          id="popular_stores"
+        ></span>
+      </h3>
       <div className="section_stores__element">
-        <Splide
-          options={{
-            type: "slide",
-            pagination: false,
-            perPage: 3,
-            perMove: 1,
-            speed: 400,
-            interval: 3,
-            breakpoints: {
-              1330: {
-                perPage: 2,
-              },
-              948: {
-                perPage: 1,
-              },
-            },
-          }}
-        >
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/5.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/6.png" alt="" />
-                <img src="/images/landing/store/7.png" alt="" />
-                <img src="/images/landing/store/8.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/1.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/2.png" alt="" />
-                <img src="/images/landing/store/3.png" alt="" />
-                <img src="/images/landing/store/4.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section_stores__element_card" tabIndex="0">
-              <div className="section_stores__element_card__viewer">
-                <img src="/images/landing/store/9.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__grid">
-                <img src="/images/landing/store/10.png" alt="" />
-                <img src="/images/landing/store/11.png" alt="" />
-                <img src="/images/landing/store/12.png" alt="" />
-              </div>
-              <div className="section_stores__element_card__profile">
-                <img src="/images/landing/store/profile.png" alt="" />
-                <div className="section_stores__element_card__profile_details">
-                  <h4>Lorem ipsum dolor sit </h4>
-                  <p>
-                    Consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
+        <Splide options={sliderOption}>
+          {React.Children.toArray(
+            popularStore.map((data) => (
+              <SplideSlide>
+                <StoreBox
+                  viewerImage={data.viewerImage}
+                  gridImage1={data.imageGrid[0]}
+                  gridImage2={data.imageGrid[1]}
+                  gridImage3={data.imageGrid[2]}
+                  profileImage={data.profileImage}
+                  name={data.name}
+                  description={data.description}
+                />
+              </SplideSlide>
+            ))
+          )}
         </Splide>
       </div>
     </section>
