2634c79055f9eda132bfdf8325444005a092f036
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-02 13:38:32 +0600

    🛠updated

--- src/pages/Pricing/AmplifyNFTGame.js
+++ src/pages/Pricing/AmplifyNFTGame.js
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { StoreBox } from "../../components/StoreBox";
 
 export default function AmplifyNFTGame() {
   const { t } = useTranslation();
@@ -9,7 +10,11 @@ export default function AmplifyNFTGame() {
         <h1>{t("AMPLIFY_NFT_GAME_TITLE")}</h1>
         <p>{t("AMPLIFY_NFT_GAME_DESCRIPTION")}</p>
       </div>
-      <div className="section_amplifyNFTGame__element"></div>
+      <div className="section_amplifyNFTGame__element">
+        <StoreBox />
+        <StoreBox />
+        <StoreBox />
+      </div>
     </section>
   );
 }
