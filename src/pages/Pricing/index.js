f3f399acd28cd8075a05e986b8442ac21fb2cba5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-02 13:40:26 +0600

    🛠section added

--- src/pages/Pricing/index.js
+++ src/pages/Pricing/index.js
@@ -3,7 +3,12 @@ import DATA from "../../data/pricingData.json";
 
 import AmplifyNFTGame from "./AmplifyNFTGame";
 import LowCostMint from "./LowCostMint";
-import LowestTransictionCosts from "./LowestTransictionCosts";
+import LowestTransactionCosts from "./LowestTransactionCosts";
+import JumpRightIn from "./JumpRightIn";
+import NeedConvencing from "../Home/NeedConvencing";
+import GetPaid from "./GetPaid";
+import LetsAmplify from "../Home/LetsAmplify";
+import AcceptedCurrencies from "./AcceptedCurrencies";
 
 export default function Pricing() {
   return (
@@ -11,7 +16,14 @@ export default function Pricing() {
       {DATA.amplifyNFTGame.visibility === true ? <AmplifyNFTGame /> : undefined}
       {DATA.lowCostMint.visibility === true ? <LowCostMint /> : undefined}
       {DATA.lowestTransactionCosts.visibility === true ? (
-        <LowestTransictionCosts />
+        <LowestTransactionCosts />
+      ) : undefined}
+      {DATA.jumpRightIn.visibility === true ? <JumpRightIn /> : undefined}
+      {DATA.convincing.visibility === true ? <NeedConvencing /> : undefined}
+      {DATA.getPaid.visibility === true ? <GetPaid /> : undefined}
+      {DATA.letsAmplify.visibility === true ? <LetsAmplify /> : undefined}
+      {DATA.acceptedCurrencies.visibility === true ? (
+        <AcceptedCurrencies />
       ) : undefined}
     </PricingContainer>
   );
