1ee480975b8aaa54fb46d8b7ccbc296ee30b4f31
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-02 13:39:53 +0600

    🔖added

--- src/pages/Pricing/LowestTransactionCosts.js
+++ src/pages/Pricing/LowestTransactionCosts.js
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-export default function LowestTransictionCosts() {
+export default function LowestTransactionCosts() {
   const { t } = useTranslation();
 
   return (
