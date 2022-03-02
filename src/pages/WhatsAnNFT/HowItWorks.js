96861290016faf04ef313c213e7840b4096c66e5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-02 19:17:36 +0600

    🔖SECTION ADDED

---
+++ src/pages/WhatsAnNFT/HowItWorks.js
@@ -0,0 +1,34 @@
+import { useTranslation } from "react-i18next";
+
+export default function HowItWorks() {
+  const { t } = useTranslation();
+
+  return (
+    <section className="section_howItWorks">
+      <h2>{t("HOW_IT_WORKS_TITLE")}</h2>
+      <p>{t("HOW_IT_WORKS_DESCRIPTION")}</p>
+      <div className="section_howItWorks__element">
+        <div className="section_howItWorks__element_option">
+          <img src="/images/landing/section/create_account.png" alt="" />
+          <h3>{t("HOW_IT_WORKS_OPTION_1_1_TITLE")}</h3>
+          <p>{t("HOW_IT_WORKS_OPTION_1_1_DESC")}</p>
+        </div>
+        <div className="section_howItWorks__element_option">
+          <img src="/images/landing/section/create_store.png" alt="" />
+          <h3>{t("HOW_IT_WORKS_OPTION_1_2_TITLE")}</h3>
+          <p>{t("HOW_IT_WORKS_OPTION_1_2_DESC")}</p>
+        </div>
+        <div className="section_howItWorks__element_option">
+          <img src="/images/landing/section/publish.png" alt="" />
+          <h3>{t("HOW_IT_WORKS_OPTION_1_3_TITLE")}</h3>
+          <p>{t("HOW_IT_WORKS_OPTION_1_3_DESC")}</p>
+        </div>
+        <div className="section_howItWorks__element_option">
+          <img src="/images/landing/section/sell.png" alt="" />
+          <h3>{t("HOW_IT_WORKS_OPTION_1_4_TITLE")}</h3>
+          <p>{t("HOW_IT_WORKS_OPTION_1_4_DESC")}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
