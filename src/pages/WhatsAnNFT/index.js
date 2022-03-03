dc43eba9c5e76b3affeb9e743b1b7eedcc79246c
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-03 23:57:57 +0600

    🛠fixation

--- src/pages/WhatsAnNFT/index.js
+++ src/pages/WhatsAnNFT/index.js
@@ -8,6 +8,9 @@ import USDSpent from "./USDSpent";
 import LetsAmplify from "../Home/LetsAmplify";
 import HowItWorks from "./HowItWorks";
 import LowCostMint from "../Pricing/LowCostMint";
+import MintYourNFT from "./MintYourNFT";
+import PreMintNFT from "./PreMintNFT";
+import CommunityGuidelines from "./CommunityGuidelines";
 
 export default function WhatsAnNFT() {
   return (
@@ -19,6 +22,11 @@ export default function WhatsAnNFT() {
       {DATA.letsAmplify.visibility === true ? <LetsAmplify /> : undefined}
       {DATA.howItWorks.visibility === true ? <HowItWorks /> : undefined}
       {DATA.lowCostMint.visibility === true ? <LowCostMint /> : undefined}
+      {DATA.mintYourNFT.visibility === true ? <MintYourNFT /> : undefined}
+      {DATA.preMintNFT.visibility === true ? <PreMintNFT /> : undefined}
+      {DATA.communityGuidelines.visibility === true ? (
+        <CommunityGuidelines />
+      ) : undefined}
     </WhatsAnNFTContainer>
   );
 }
