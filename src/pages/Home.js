26183b82de7080c105eac0cd9a0a83d655c5b201
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-31 13:40:00 +0600

    ✨🛠team section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
+import React, { useState } from "react";
 import { HomeContainer } from "../styles/pages/HomeStyle.js";
 import DATA from "../data/homeData.json";
-import { ButtonPrimary, ButtonTertiary } from "../components/Buttons.js";
+import { teamMemberData } from "../data/teamData.js";
+import {
+  ButtonPrimary,
+  ButtonSecondary,
+  ButtonTertiary,
+} from "../components/Buttons.js";
 import UList from "../styles/components/UList.js";
-import React, { useState } from "react";
+import TeamMemberCard from "../components/TeamMemberCard.js";
+
+console.table(teamMemberData);
 
 export default function Home() {
   const [section5, setSection5] = useState(DATA.section5.options[0].name);
@@ -203,6 +211,75 @@ export default function Home() {
         {/* <p className="am_diff__link">
           {DATA.amDif.p_l2} <a href={DATA.amDif.href}>{DATA.amDif.link}</a>
         </p> */}
+      </section>
+      <section className="section_team">
+        <h2>Meet the team</h2>
+        <p>Get to know the AssetMantle team </p>
+        <div className="section_team__container">
+          {teamMemberData[0] && (
+            <TeamMemberCard
+              imgSrc={
+                teamMemberData[0].image !== null &&
+                teamMemberData[0].image !== undefined &&
+                teamMemberData[0].image !== ""
+                  ? teamMemberData[0].image
+                  : "placeholder.png"
+              }
+              name={teamMemberData[0].name}
+              position={teamMemberData[0].designation}
+              social={teamMemberData[0].social}
+              key={teamMemberData[0].name + teamMemberData[0].designation}
+            />
+          )}
+          {teamMemberData[1] && (
+            <TeamMemberCard
+              imgSrc={
+                teamMemberData[1].image !== null &&
+                teamMemberData[1].image !== undefined &&
+                teamMemberData[1].image !== ""
+                  ? teamMemberData[1].image
+                  : "placeholder.png"
+              }
+              name={teamMemberData[1].name}
+              position={teamMemberData[1].designation}
+              social={teamMemberData[1].social}
+              key={teamMemberData[1].name + teamMemberData[1].designation}
+            />
+          )}
+          {teamMemberData[2] && (
+            <TeamMemberCard
+              imgSrc={
+                teamMemberData[2].image !== null &&
+                teamMemberData[2].image !== undefined &&
+                teamMemberData[2].image !== ""
+                  ? teamMemberData[2].image
+                  : "placeholder.png"
+              }
+              name={teamMemberData[2].name}
+              position={teamMemberData[2].designation}
+              social={teamMemberData[2].social}
+              key={teamMemberData[2].name + teamMemberData[2].designation}
+            />
+          )}
+          {teamMemberData[3] && (
+            <TeamMemberCard
+              imgSrc={
+                teamMemberData[3].image !== null &&
+                teamMemberData[3].image !== undefined &&
+                teamMemberData[3].image !== ""
+                  ? teamMemberData[3].image
+                  : "placeholder.png"
+              }
+              name={teamMemberData[3].name}
+              position={teamMemberData[3].designation}
+              social={teamMemberData[3].social}
+              key={teamMemberData[3].name + teamMemberData[3].designation}
+            />
+          )}
+        </div>
+        <div className="section_team__button">
+          <ButtonSecondary text="View More" href="/about#team" />
+        </div>
       </section>
       <section className="section_source">
         <span id="Source"></span>
