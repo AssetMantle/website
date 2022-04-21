9f33fd12811e24ea1729781a0b286cbea5cf09c9
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-21 19:23:29 +0600

    🛠🦄bg and bg assets added

---
+++ src/styles/AppStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  background-color: var(--bg);
+  background-image: url("/images/bg/bg_big.webp");
+  background-size: 100%;
+  background-repeat: no-repeat repeat;
+`;
+
+export default AppContainer;
