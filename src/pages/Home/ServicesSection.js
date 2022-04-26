3e799f4e6cf67c5c4b0399829e6fa3679449158b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-26 15:05:24 +0600

    🛠✨updated

--- src/pages/Home/ServicesSection.js
+++ src/pages/Home/ServicesSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ButtonTertiary } from "../../components/Buttons";
+import { ButtonPrimary, ButtonSecondary } from "../../components/Buttons";
 import DATA from "../../data/homeData.json";
 import { useTranslation } from "react-i18next";
 
@@ -9,6 +9,18 @@ const ServicesSection = () => {
   return (
     <section className="section_services">
       <div className="section_services__element">
+        <img src="/images/landing/section/bags.png" alt="bags  illustration" />
+        <div className="section_services__element_details">
+          <h2>{t("SERVICES_2_TITLE")}</h2>
+          <p>{t("SERVICES_2_DESCRIPTION")}</p>
+          <ButtonPrimary
+            href={DATA.services.rightHref && DATA.services.rightHref}
+            text={t("SERVICES_2_BUTTONTEXT")}
+            comingSoon={true}
+          />
+        </div>
+      </div>
+      <div className="section_services__element">
         <img
           src="/images/landing/section/bricks.png"
           alt="blocks  illustration"
@@ -16,23 +28,9 @@ const ServicesSection = () => {
         <div className="section_services__element_details">
           <h2>{t("SERVICES_1_TITLE")}</h2>
           <p>{t("SERVICES_1_DESCRIPTION")}</p>
-          <ButtonTertiary
+          <ButtonSecondary
             href={DATA.services.leftHref && DATA.services.leftHref}
             text={t("SERVICES_1_BUTTONTEXT")}
-            leftArrow
-            comingSoon={true}
-          />
-        </div>
-      </div>
-      <div className="section_services__element">
-        <img src="/images/landing/section/bags.png" alt="bags  illustration" />
-        <div className="section_services__element_details">
-          <h2>{t("SERVICES_2_TITLE")}</h2>
-          <p>{t("SERVICES_2_DESCRIPTION")}</p>
-          <ButtonTertiary
-            href={DATA.services.rightHref && DATA.services.rightHref}
-            text={t("SERVICES_2_BUTTONTEXT")}
-            leftArrow
             comingSoon={true}
           />
         </div>
