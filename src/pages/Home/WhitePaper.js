863f6711dde75154126f51cbcb75266a0540a5cb
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-29 14:24:06 +0600

    🛠✨🔖added and completed

---
+++ src/pages/Home/WhitePaper.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ButtonSecondary } from "../../components/Buttons";
+import DATA from "../../data/homeData.json";
+import { useTranslation } from "react-i18next";
+
+export default function WhitePaper() {
+  const { t } = useTranslation();
+
+  return (
+    <section className="section_whitepaper">
+      <div className="section_whitepaper__element">
+        <h2>{t("WHITEPAPER_TITLE")}</h2>
+        <p>{t("WHITEPAPER_DESCRIPTION")}</p>
+      </div>
+      <div className="section_whitepaper__element bu">
+        <ButtonSecondary
+          text={t("WHITEPAPER_BUTTON_TEXT")}
+          href={DATA.whitepaper.button.href && DATA.whitepaper.button.href}
+          targetBlank={true}
+        />
+      </div>
+    </section>
+  )
+}
