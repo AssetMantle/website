e0d41a5fc6f066003c502af93656a8088ed2769e
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-29 14:23:13 +0600

    🦄✨🛠done

--- src/pages/Home/AirdropSection.js
+++ src/pages/Home/AirdropSection.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import DATA from "../../data/homeData.json";
+import { ButtonPrimary } from "../../components/Buttons";
+import { useTranslation } from "react-i18next";
 
 export default function AirdropSection() {
+  const { t } = useTranslation();
+
   return (
-    <div>AirdropSection</div>
+    <section className="section_airdrop">
+      <div className="section_airdrop__element">
+        <img
+          src={`/images/landing/section/png/token.png`}
+          alt="roadmap illustration"
+        />
+      </div>
+      <div className="section_airdrop__element">
+        <h2 className="yellow-t">{t("DROP_TITLE")}</h2>
+        <p>{t("DROP_DESCRIPTION")}</p>
+      </div>
+      <div className="section_airdrop__element bu">
+        <ButtonPrimary
+          text={t("DROP_BUTTON_TEXT")}
+          href={DATA.drop.button.href && DATA.drop.button.href}
+        />
+      </div>
+    </section>
   )
 }
