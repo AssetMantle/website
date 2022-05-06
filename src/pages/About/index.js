939b22b69094c07edb94fc8588b08d2a59402123
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-05-06 12:25:48 +0600

    🛠️comments removed

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { AboutContainer } from "../../styles/pages/AboutContainer";
-// import { BsFillCaretRightFill } from "react-icons/bs";
 import TeamMemberCard from "../../components/TeamMemberCard";
 import { teamMemberData } from "../../data/teamData";
 import { useTranslation } from "react-i18next";
@@ -32,63 +31,12 @@ export default function About() {
         <div className="section_whyBetter__image">
           <img src={aboutData.vision.image} alt={aboutData.vision.image_alt} />
         </div>
-        {/* <ul className="section_whyBetter__list">
-          <li>
-            <span className="li">
-              <BsFillCaretRightFill />
-            </span>
-            <p>
-              Sovereignity of store
-              <span className="li__details">
-                As a store owner, you will enjoy a high degree of controlling
-                power over your products, terms, and finances.
-              </span>
-            </p>
-          </li>
-          <li>
-            <span className="li">
-              <BsFillCaretRightFill />
-            </span>
-            <p>
-              Transferability of assets
-              <span className="li__details">
-                With AssetMantle, you can freely transfer your digital assets
-                across chains and platforms without hassles.
-              </span>
-            </p>
-          </li>
-          <li>
-            <span className="li">
-              <BsFillCaretRightFill />
-            </span>
-            <p>
-              Agnosticism of currency
-              <span className="li__details">
-                Asset Mantle supports a variety of web3 wallets including Keplr
-                and hardware wallets like Ledger.
-              </span>
-            </p>
-          </li>
-          <li>
-            <span className="li">
-              <BsFillCaretRightFill />
-            </span>
-            <p>
-              Customisability of metadata
-              <span className="li__details">
-                No more getting stuck with rigid parameters. You can customize
-                your meta data as per your specific products and needs.
-              </span>
-            </p>
-          </li>
-        </ul> */}
       </section>
       <section className="section_team" id="team">
         <h2>{t("ABOUT_VISION_TITLE")}</h2>
         <p>
           {t("ABOUT_TEAM_DESCRIPTION")}
         </p>
-        {/* <h3>Team</h3> */}
         <div className="section_team__container">
           {Array.isArray(teamMemberData) &&
             teamMemberData.length !== 0 &&
