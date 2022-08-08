1458888b49425f5c810e8b9e6e30bd0553f39201
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-08 19:55:55 +0600

    📕💅populated with data and fixed some styling

--- views/GallerySection.js
+++ views/GallerySection.js
@@ -12,17 +12,20 @@ import { Container } from "@mui/system";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const GalleryConfigData = {
-  textAlign: "center",
-  title: "",
-  titleComponent: "",
-  titleVariant: "",
-  description: "",
-  descriptionStyle: false, // object of styles or false
+  textAlign: "", // >>> default: left
+  title: "our products",
+  titleComponent: "h2",
+  titleVariant: "h1",
+  description:
+    "AssetMantle’s suite of products help you up your game of digital asset ownership",
+  descriptionStyle: {
+    maxWidth: "480px",
+    mt: 6,
+  }, // object of styles or false
   galleries: [
     // object template
     // {
-    //   image: "",
-    //   height: "",
+    //   image: "/images/mantleplace.png",
     //   title: "",
     //   description: "",
     //   buttons: [
@@ -32,29 +35,80 @@ const GalleryConfigData = {
     //     },
     //   ],
     // },
+    {
+      image: "/images/MantlePlace.png",
+      title: "MantlePlace",
+      description:
+        "A new, highly optimized NFT marketplace for Cosmos Ecosystem",
+      buttons: [
+        {
+          text: "Learn More",
+          url: "",
+        },
+      ],
+    },
+    {
+      image: "/images/MantleBuilder.png",
+      title: "MantleBuilder",
+      description:
+        "A no-code marketplace builder enabling instant NFT shopifying",
+      buttons: [
+        {
+          text: "Learn More",
+          url: "",
+        },
+      ],
+    },
+    {
+      image: "/images/MantleWallet.png",
+      title: "MantleWallet",
+      description: "A non-custodial blockchain wallet for AssetMantle chain",
+      buttons: [
+        {
+          text: "Learn More",
+          url: "",
+        },
+      ],
+    },
+    {
+      image: "/images/MantleExplorer.png",
+      title: "MantleExplorer",
+      description: "A blockchain explorer for the AssetMantle chain",
+      buttons: [
+        {
+          text: "Learn More",
+          url: "",
+        },
+      ],
+    },
   ],
 };
 
 const sectionStyle = {
   my: 10,
+  py: 4,
   paddingTop: "7px",
   color: "primary.light",
   textAlign: { xs: "center", sm: "center", md: "left" },
 };
-
 const optionContainerStyles = {
   display: "flex",
-  justifyContent: { sm: "center", md: "space-between" },
+  justifyContent: "center",
   flexWrap: "wrap",
-  gap: "30px",
-  pt: 4,
+  gap: "21px",
+  pt: 8,
 };
 const optionStyles = {
-  borderRadius: "6px",
-  backgroundColor: "transparent",
-  maxWidth: 262,
+  background:
+    "linear-gradient(157.26deg, rgba(217, 217, 217, 0.24) -5.39%, rgba(217, 217, 217, 0.12) 108.05%)",
+  backdropFilter: "blur(47.4479px)",
+  borderRadius: "16px",
+  maxWidth: 272,
   boxShadow: "none",
+  padding: "0 11px",
   textAlign: { xs: "center", md: "left" },
+  display: "flex",
+  flexDirection: "column",
 };
 
 export default function GallerySection() {
@@ -81,9 +135,6 @@ export default function GallerySection() {
                 : "h2"
             }
             color="inherit"
-            sx={{
-              pb: 2,
-            }}
           >
             {GalleryConfigData.title}
           </Typography>
@@ -91,7 +142,7 @@ export default function GallerySection() {
         {GalleryConfigData.description && (
           <Typography
             component="p"
-            variant="subtitle1"
+            variant="body1"
             color="inherit"
             sx={
               GalleryConfigData.descriptionStyle
@@ -108,33 +159,37 @@ export default function GallerySection() {
           <Box sx={optionContainerStyles}>
             {GalleryConfigData.galleries.map((Gallery, index) => (
               <Card sx={optionStyles}>
-                <CardMedia
-                  key={index}
-                  component="img"
-                  alt={Gallery.title}
-                  height={Gallery.height ? Gallery.height : 420}
-                  image={Gallery.image}
-                  sx={{ border: "none" }}
-                />
-                <CardContent>
-                  {Gallery.title && (
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      color="primary.light"
-                    >
-                      {Gallery.title}
-                    </Typography>
-                  )}
-                  {Gallery.description && (
-                    <Typography variant="body2" color="primary.light">
-                      {Gallery.description}
-                    </Typography>
-                  )}
-                </CardContent>
+                {Gallery.image && (
+                  <CardMedia
+                    key={index}
+                    component="img"
+                    alt={Gallery.title}
+                    // height={Gallery.height ? Gallery.height : 420}
+                    image={Gallery.image}
+                    sx={{ border: "none", width: "100%", aspectRatio: "1/1" }}
+                  />
+                )}
+                {(Gallery.title || Gallery.description) && (
+                  <CardContent>
+                    {Gallery.title && (
+                      <Typography
+                        gutterBottom
+                        variant="h4"
+                        component="h4"
+                        color="primary.light"
+                      >
+                        {Gallery.title}
+                      </Typography>
+                    )}
+                    {Gallery.description && (
+                      <Typography variant="body2" color="primary.light">
+                        {Gallery.description}
+                      </Typography>
+                    )}
+                  </CardContent>
+                )}
                 {Gallery.buttons.length !== 0 && (
-                  <CardActions>
+                  <CardActions sx={{ marginTop: "auto" }}>
                     {Gallery.buttons.map((button, index) => (
                       <Button
                         component="a"
