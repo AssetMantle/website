ff143b627570cb6fe6404f2d4bed89189996f143
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-08 20:29:57 +0600

    📕💅populated with data and fixed some styles

--- views/FeatureSection.js
+++ views/FeatureSection.js
@@ -1,41 +1,106 @@
-import { Box, CardMedia, Typography } from "@mui/material";
+import { Box, Card, CardMedia, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import HubOutlinedIcon from "@mui/icons-material/HubOutlined";
+import MoneyOffIcon from "@mui/icons-material/MoneyOff";
+import FastForwardOutlinedIcon from "@mui/icons-material/FastForwardOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import SwapHorizOutlinedIcon from "@mui/icons-material/SwapHorizOutlined";
 
 const FeatureConfigData = {
   textAlign: "center",
-  title: "Get mixin'",
+  title: "Why choose us?",
   titleComponent: "",
   titleVariant: "h1",
-  description: "",
-  descriptionStyle: false, // object of styles or false
+  description:
+    "AssetMantle’s foundation is built using the best-in-class core blockchain principles, and optimized for the next evolution of NFT economy",
+  descriptionStyle: { maxWidth: "min(836px, 100%)", margin: "0 auto" }, // object of styles or false
   features: [
     {
       href: "",
-      icon: "https://uploads-ssl.webflow.com/623a0c9828949e55356286f9/623b5936a09ba064d3f5c894_Microscope.svg",
-      title: "Analyze",
+      icon: (
+        <CategoryOutlinedIcon
+          sx={{ fontSize: "56px", color: "primary.main" }}
+        />
+      ), // put icon component or url >>don't forgot to change the below
+      iconType: "component", // >>>>put nothing if icon is a img or url
+      title: "Optimized for NFTs",
+      titleVariant: "h2",
       description:
-        "Science begins with observation. Dig into charts and data on assets, liquidity.",
+        "We’re building a suite of NFT products, with the chain specifically calibrated with features like Verified NFT Creators, customized NFT properties, in-built whitelisting features etc.",
+      descriptionVariant: "body1",
     },
     {
       href: "",
-      icon: "https://uploads-ssl.webflow.com/623a0c9828949e55356286f9/623b5b5e05aa442fc4394245_Govern%20icon.svg",
-      title: "Govern",
+      icon: (
+        <HubOutlinedIcon sx={{ fontSize: "56px", color: "primary.main" }} />
+      ), // put icon component or url >>don't forgot to change the below
+      iconType: "component", // >>>>put nothing if icon is a img or url
+      title: "Pioneering Cosmos Ecosystem",
+      titleVariant: "h2",
       description:
-        "We all steer the ship together. Explore proposals and discuss upcoming experiments.",
+        "AssetMantle being one of the top zones of the Cosmos Hub, leverages features like Tendermint Core for Consensus and a highly modular SDK framework.",
+      descriptionVariant: "body1",
     },
     {
       href: "",
-      icon: "https://uploads-ssl.webflow.com/623a0c9828949e55356286f9/623b5bc8b3d885802355c0c1_Build%20icon.svg",
-      title: "Build",
+      icon: <MoneyOffIcon sx={{ fontSize: "56px", color: "primary.main" }} />, // put icon component or url >>don't forgot to change the below
+      iconType: "component", // >>>>put nothing if icon is a img or url
+      title: "Extremely Low Fees",
+      titleVariant: "h2",
       description:
-        "Roll up your sleeves. Build interchain-native apps and smart contracts.",
+        "This network offers the basis for an economical network, with gas fees as low as $0.01 per transaction, it has the lowest transaction fees compared to any popular network.",
+      descriptionVariant: "body1",
+    },
+    {
+      href: "",
+      icon: (
+        <FastForwardOutlinedIcon
+          sx={{ fontSize: "56px", color: "primary.main" }}
+        />
+      ), // put icon component or url >>don't forgot to change the below
+      iconType: "component", // >>>>put nothing if icon is a img or url
+      title: "Fast Order Fulfillments",
+      titleVariant: "h2",
+      description:
+        "Extremely fast transactions nearing 7 seconds enables almost zero waiting time for fulfillment of orders of buy, sell, auction etc.",
+      descriptionVariant: "body1",
+    },
+    {
+      href: "",
+      icon: (
+        <SecurityOutlinedIcon
+          sx={{ fontSize: "56px", color: "primary.main" }}
+        />
+      ), // put icon component or url >>don't forgot to change the below
+      iconType: "component", // >>>>put nothing if icon is a img or url
+      title: "Completely Secure",
+      titleVariant: "h2",
+      description:
+        "With high level of network security, and leveraging the native token $MNTL, AssetMantle can provide the level of protection required for your asset ownership.",
+      descriptionVariant: "body1",
+    },
+    {
+      href: "",
+      icon: (
+        <SwapHorizOutlinedIcon
+          sx={{ fontSize: "56px", color: "primary.main" }}
+        />
+      ), // put icon component or url >>don't forgot to change the below
+      iconType: "component", // >>>>put nothing if icon is a img or url
+      title: "Future Interoperability",
+      titleVariant: "h2",
+      description:
+        "InterNFT can potentially enable NFTs to be transferred to other connected chains, thereby creating a larger shared market economy.",
+      descriptionVariant: "body1",
     },
   ],
 };
 
 const sectionStyle = {
   my: 10,
+  py: 4,
   color: "primary.light",
   textAlign: { xs: "center", sm: "center", md: "left" },
 };
@@ -43,19 +108,23 @@ const sectionStyle = {
 const optionContainerStyles = {
   display: "flex",
   alignItems: "center",
-  justifyContent: { xs: "center", sm: "center", md: "space-around" },
-  flexWrap: { xs: "wrap", md: "nowrap" },
-  gap: "28px",
-  mt: 4,
+  justifyContent: "center",
+  flexWrap: "wrap",
+  gap: "14px",
+  mt: 5,
 };
 const optionStyles = {
-  width: "min(360px,100%)",
-  height: "100%",
-  backgroundColor: "secondary.dark",
+  background:
+    "linear-gradient(157.26deg, rgba(217, 217, 217, 0.24) -5.39%, rgba(217, 217, 217, 0.12) 108.05%)",
+  backdropFilter: "blur(47.4479px)",
+  borderRadius: "16px",
+  maxWidth: 374,
+  boxShadow: "none",
+  padding: "16px 22px",
   borderRadius: "25px",
-  textAlign: "left",
-  padding: "28px 30px 39px",
+  minHeight: "265px",
   transition: "all 0.3s ease-in-out",
+  color: "primary.light",
   "&:hover": {
     backgroundColor: "navBg",
   },
@@ -95,7 +164,7 @@ export default function FeatureSection() {
         {FeatureConfigData.description && (
           <Typography
             component="p"
-            variant="subtitle1"
+            variant="body1"
             color="inherit"
             sx={
               FeatureConfigData.descriptionStyle
@@ -111,33 +180,42 @@ export default function FeatureSection() {
         {FeatureConfigData.features && FeatureConfigData.features.length > 0 && (
           <Box sx={optionContainerStyles}>
             {FeatureConfigData.features.map((feature, index) => (
-              <Box sx={optionStyles} key={index}>
+              <Card sx={optionStyles} key={index}>
                 <Box
                   sx={{
                     display: "flex",
-                    alignItems: "flex-end",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
                     gap: 2,
-                    pb: 3,
+                    pb: 2,
                   }}
                 >
-                  {feature.icon && (
-                    <CardMedia
-                      sx={{ width: 32, aspectRatio: "1/1" }}
-                      image={feature.icon}
-                    />
-                  )}
+                  {feature.iconType === "component"
+                    ? feature.icon && feature.icon
+                    : feature.icon && (
+                        <CardMedia
+                          sx={{ width: 32, aspectRatio: "1/1" }}
+                          image={feature.icon}
+                        />
+                      )}
                   {feature.title && (
-                    <Typography component="h3" variant="h3" color="inherit">
+                    <Typography
+                      component="h3"
+                      variant="h5"
+                      color="inherit"
+                      sx={{ fontWeight: "700" }}
+                    >
                       {feature.title}
                     </Typography>
                   )}
                 </Box>
                 {feature.description && (
-                  <Typography component="p" variant="body1" color="inherit">
+                  <Typography component="p" variant="body2" color="inherit">
                     {feature.description}
                   </Typography>
                 )}
-              </Box>
+              </Card>
             ))}
           </Box>
         )}
