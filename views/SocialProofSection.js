5a3a49e398c3d3deb1e9634b4e32affb7f1a19bd
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-11 20:11:39 +0600

    💅box shadow updated to light

--- views/SocialProofSection.js
+++ views/SocialProofSection.js
@@ -118,8 +118,9 @@ export default function SocialProofSection() {
                     backgroundColor: "transparent",
                     height: "100%",
                     display: "flex",
+                    boxShadow: "none",
                     "&:hover": {
-                      boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.6)",
+                      boxShadow: "0px 0px 4px 4px rgba(0,0,0,0.2)",
                     },
                   }}
                   key={index}
