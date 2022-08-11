a03af221c1561fa7a498efaed0b7d3fd4a51f3ca
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-11 20:00:38 +0600

    📕🛠️💅completed

--- views/SocialProofSection.js
+++ views/SocialProofSection.js
@@ -1,87 +1,143 @@
-import { Box, Grid, Link, Typography } from "@mui/material";
+import { Box, Card, CardMedia, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 
+const SocialProofConfigData = {
+  textAlign: "center",
+  title: "FEATURED IN",
+  titleComponent: "",
+  titleVariant: "h1",
+  description: "",
+  descriptionStyle: { maxWidth: "min(836px, 100%)", margin: "0 auto" }, // object of styles or false
+  features: [
+    // object template
+    // {
+    //   title: "",
+    //   image: "",
+    //   link: "",
+    // },
+    {
+      title: "yahoo finance",
+      image: "https://assetmantle.one/images/landing/yahoo_finance.png",
+      link: "https://finance.yahoo.com/news/assetmantle-launches-long-awaited-mainnet-150000066.html",
+    },
+    {
+      title: "coinquora",
+      image: "https://assetmantle.one/images/landing/coinquora.png",
+      link: "https://coinquora.com/avigation-ai-collabs-with-assetmantle-to-revamp-nfts-via-ai-integration/",
+    },
+    {
+      title: "digitaljournal",
+      image: "https://assetmantle.one/images/landing/digitaljournal.png",
+      link: "https://www.digitaljournal.com/pr/assetmantle-launches-its-long-awaited-mainnet",
+    },
+    {
+      title: "investing.com",
+      image: "https://assetmantle.one/images/landing/investing.png",
+      link: "https://m.investing.com/news/cryptocurrency-news/avigation-ai-collabs-with-assetmantle-to-revamp-nfts-via-ai-integration-2820215?ampMode=1",
+    },
+    {
+      title: "techbullion",
+      image: "https://assetmantle.one/images/landing/techbullion.png",
+      link: "https://techbullion.com/talking-nfts-with-assetmantle-founder-deepanshu-tripathi/",
+    },
+  ],
+};
+
 const sectionStyle = {
-  mt: 13,
-  paddingTop: "7px",
+  my: 10,
+  py: 4,
   color: "primary.light",
   textAlign: { xs: "center", sm: "center", md: "left" },
+};
+const optionContainerStyles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: { xs: "center", md: "space-around" },
+  flexWrap: "wrap",
+
+  pt: 10,
 };
 
 export default function SocialProofSection() {
   return (
     <Box component="section" sx={sectionStyle}>
-      <Container maxWidth="lg">
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={5}>
-            <Typography
-              component="h2"
-              variant="h2"
-              color="inherit"
-              sx={{
-                paddingBottom: "33px",
-                backgroundImage: "url(/static/line.svg)",
-                backgroundPosition: "bottom right",
-                backgroundRepeat: "no-repeat",
-                width: "min(510px,100%)",
-                "@media (max-width:900px)": {
-                  width: "100%",
-                },
-              }}
-            >
-              SocialProofSection
-            </Typography>
-            <Box sx={{ mt: 8 }}>
-              <Typography component="p" variant="body1">
-                This open collaboration was initiated by the Interchain
-                Foundation and is facilitated by ixo (The Internet of Impact)
-                together with AssetMantle.
-                <br />
-                <br />
-                <br />
-                <Link
-                  underline="hover"
-                  href="https://interchain.io/"
-                  sx={{ cursor: "pointer" }}
+      <Container
+        maxWidth="lg"
+        sx={{
+          textAlign: SocialProofConfigData.textAlign
+            ? SocialProofConfigData.textAlign
+            : "left",
+        }}
+      >
+        {SocialProofConfigData.title && (
+          <Typography
+            component={
+              SocialProofConfigData.titleComponent
+                ? SocialProofConfigData.titleComponent
+                : "h2"
+            }
+            variant={
+              SocialProofConfigData.titleVariant
+                ? SocialProofConfigData.titleVariant
+                : "h2"
+            }
+            color="inherit"
+            sx={{
+              pb: 2,
+            }}
+          >
+            {SocialProofConfigData.title}
+          </Typography>
+        )}
+        {SocialProofConfigData.description && (
+          <Typography
+            component="p"
+            variant="body1"
+            color="inherit"
+            sx={
+              SocialProofConfigData.descriptionStyle
+                ? SocialProofConfigData.descriptionStyle
+                : {
+                    pb: 2,
+                  }
+            }
+          >
+            {SocialProofConfigData.description}
+          </Typography>
+        )}
+        {Array.isArray(SocialProofConfigData.features) &&
+          SocialProofConfigData.features &&
+          SocialProofConfigData.features.length > 0 && (
+            <Box sx={optionContainerStyles}>
+              {SocialProofConfigData.features.map((feature, index) => (
+                <Card
+                  sx={{
+                    width: "min(208px,100%)",
+                    padding: 3,
+                    backgroundColor: "transparent",
+                    height: "100%",
+                    display: "flex",
+                    "&:hover": {
+                      boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.6)",
+                    },
+                  }}
+                  key={index}
+                  component="a"
+                  href={feature.link && feature.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  Interchain Foundation
-                </Link>{" "}
-                grants along with the collaborative development of developers
-                from{" "}
-                <Link
-                  underline="hover"
-                  href="https://www.w3.org/"
-                  sx={{ cursor: "pointer" }}
-                >
-                  World Wide Web Consortium (W3C)
-                </Link>{" "}
-                and other development teams worked to further expand the
-                capabilities of Cosmos SDK
-                <br />
-                <br />
-                <br />
-                <Link
-                  underline="none"
-                  href="https://blog.cosmos.network/progress-report-interchain-nft-metadata-standards-94770dfe3bb1"
-                  sx={{ cursor: "pointer" }}
-                >
-                  LEARN MORE
-                </Link>
-              </Typography>
+                  <CardMedia
+                    component="img"
+                    alt={feature.title && feature.title}
+                    image={feature.image && feature.image}
+                    sx={{ border: "none", width: "100%", height: "auto" }}
+                  />
+                </Card>
+              ))}
             </Box>
-          </Grid>
-          <Grid item xs={12} md={7}>
-            <img
-              src="/static/InterNFTGraphic.png"
-              alt=""
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-            />
-          </Grid>
-        </Grid>
+          )}
       </Container>
     </Box>
   );
