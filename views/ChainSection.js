5311db100350217d4264daf774cbddf698d85132
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-16 18:40:49 +0600

    chain section completed

--- views/ChainSection.js
+++ views/ChainSection.js
@@ -1,200 +1,228 @@
-import AssignmentIcon from "@mui/icons-material/Assignment";
-import { Box, Grid, Stack, Typography } from "@mui/material";
-import Avatar from "@mui/material/Avatar";
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import CallMadeIcon from "@mui/icons-material/CallMade";
 
-// import Icon from '@mui/material/Icon';
+const ChainSectionConfigData = {
+  textAlign: "", // >>> default: left
+  title: "$MNTL",
+  titleIcon: "/images/chain/mntl.svg", // url:"/someImage.png">>> default: none
+  titleComponent: "h2",
+  titleVariant: "h2",
+  description: "The native token which powers the AssetMantle’s NFT Economy",
+  descriptionStyle: {
+    maxWidth: "450px",
+    fontWeight: "400",
+    textAlign: { xs: "center", sm: "left" },
+    mx: { xs: "auto !important", sm: "0 !important" },
+  }, // object of styles or false
+  backgroundImage: "", // "url('/images/mantleplace.png')",
+  defaultChainDescription: "Part of Rich Cosmos Ecosystem",
+  ctas: [
+    {
+      title: "Buy Now ",
+      url: "https://app.osmosis.zone/?from=USDC&to=MNTL",
+      icon: <ArrowForwardIosIcon />, // url: "/" or component: <Icon/>
+      target: "_blank", // valid values: "_blank", "_self", "_parent", "_top" >>>default: "_self"
+      disabled: false,
+      variant: "contained",
+    },
+    {
+      title: "Airdrops",
+      url: "https://airdrop.assetmantle.one/",
+      icon: <CallMadeIcon />, // url: "/" or component: <Icon/>
+      target: "_blank", // valid values: "_blank", "_self", "_parent", "_top" >>>default: "_self"
+      disabled: false,
+      variant: "outlined",
+    },
+  ],
+  chains: [
+    {
+      icon: "/images/chain/modular.svg", // add url: "https://icon.png"
+      title: "Inherits modular framework of Cosmos SDK",
+    },
+    {
+      icon: "/images/chain/cosmos.svg", // add url: "https://icon.png"
+      title: "Imbibes the leading BFT Consensus Engine",
+    },
+    {
+      icon: "/images/chain/connected.svg", // add url: "https://icon.png"
+      title: "Implements connectedness across other Cosmos Zones",
+    },
+    {
+      icon: "/images/chain/interNft.svg", // add url: "https://icon.png"
+      title: "Instills the plan to build open standards for interchain NFT",
+    },
+  ],
+};
 
 const sectionStyle = {
+  my: 10,
   py: 4,
-  mb: 4,
   color: "primary.light",
-  textAlign: { xs: "center", sm: "center", md: "center" },
-  backgroundColor: "pink",
+  textAlign: { xs: "center", sm: "center", md: "left" },
 };
 
 export default function ChainSection() {
-  const LinksJSX = (
-    <Stack
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <AssignmentIcon fontSize="large" />
-      <Typography variant="h2" color="primary">
-        $MNTL
-      </Typography>
-    </Stack>
-  );
-
-  const TokenTickerJSX = (
-    <Stack
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <AssignmentIcon fontSize="large" />
-      <Typography variant="h1" color="primary">
-        $MNTL
-      </Typography>
-    </Stack>
-  );
-
-  // contents of left sub-section
-  const LeftSubSectionJSX = (
-    // Stack with Icons Grid & Chain Description
-    <Stack
-      direction="column"
-      // justifyContent="center"
-      alignItems="center"
-      spacing={2}
-      sx={{
-        backgroundColor: "green",
-      }}
-    >
-      {/* Icon Grid */}
-      <Grid
-        container
-        // spacing={2}
-        sx={{
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "green",
-        }}
-      >
-        {/* Icons */}
-        <Grid
-          item
-          xs={6}
-          md={6}
-          sx={{ backgroundColor: "rgba(0, 127, 127, 0.8)" }}
-        >
-          <Avatar variant="rounded" sx={{ width: "86", height: "56" }}>
-            <AssignmentIcon />
-          </Avatar>
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          md={6}
-          sx={{ backgroundColor: "rgba(0, 127, 127, 0.8)" }}
-        >
-          <Avatar variant="rounded" sx={{ width: "86", height: "56" }}>
-            <AssignmentIcon />
-          </Avatar>
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          md={6}
-          sx={{ backgroundColor: "rgba(0, 127, 127, 0.8)" }}
-        >
-          <Avatar variant="rounded" sx={{ width: "86", height: "56" }}>
-            <AssignmentIcon />
-          </Avatar>
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          md={6}
-          sx={{ backgroundColor: "rgba(0, 127, 127, 0.8)" }}
-        >
-          <Avatar variant="rounded" sx={{ width: "86", height: "56" }}>
-            <AssignmentIcon />
-          </Avatar>
-        </Grid>
-      </Grid>
-
-      {/* Chain Description */}
-      <Typography
-        variant="body1"
-        color="primary"
-        align="center"
-        sx={{ maxWidth: 300, backgroundColor: "yellowgreen" }}
-      >
-        Part of rich Cosmos Ecosystem
-      </Typography>
-    </Stack>
-  );
-
-  // contents of right sub-section
-  const RightSubSectionJSX = (
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-    >
-      {/* Token Icon & Ticker */}
-      {TokenTickerJSX}
-
-      {/* Token Description */}
-      <Typography
-        variant="body1"
-        color="primary"
-        align="center"
-        sx={{ maxWidth: 300 }}
-      >
-        The native token which powers the AssetMantle’s NFT Economy
-      </Typography>
-
-      {/* Links */}
-      {LinksJSX}
-    </Stack>
-  );
+  const [showDescription, setShowDescription] = React.useState();
 
   return (
     <Box component="section" sx={sectionStyle}>
-      <Container maxWidth="lg" disableGutters>
-        {/* Stack with Title & Section Contents */}
-        <Stack
-          direction="column"
-          // justifyContent="center"
-          // alignItems="center"
-          spacing={4}
-          sx={{ backgroundColor: "orange" }}
-        >
-          {/* Title */}
-          <Typography
-            variant="h1"
-            color="primary"
-            sx={{ backgroundColor: "blue" }}
-          >
-            The AssetMantle Chain
-          </Typography>
-
-          {/* Section Contents */}
-          <Grid container>
-            {/* Left Sub-section */}
-            <Grid
-              item
-              xs={12}
-              md={6}
-              sm={6}
-              lg={6}
-              xl={6}
-              sx={{ backgroundColor: "aliceblue" }}
-            >
-              {LeftSubSectionJSX}
-            </Grid>
-
-            {/* Right Sub-section */}
-            <Grid
-              item
-              xs={12}
-              md={6}
-              sm={6}
-              lg={6}
-              xl={6}
-              sx={{ backgroundColor: "red" }}
-            >
-              {RightSubSectionJSX}
-            </Grid>
+      <Container
+        maxWidth="lg"
+        sx={{
+          textAlign: ChainSectionConfigData.textAlign
+            ? ChainSectionConfigData.textAlign
+            : "left",
+        }}
+      >
+        <Grid container spacing={2}>
+          {/* Left Sub-section */}
+          <Grid item xs={12} sm={6}>
+            <Stack spacing={2}>
+              {Array.isArray(ChainSectionConfigData.chains) &&
+                ChainSectionConfigData.chains &&
+                ChainSectionConfigData.chains.length > 0 && (
+                  <Grid container>
+                    {ChainSectionConfigData.chains.map((data, index) => (
+                      <Grid
+                        item
+                        xs={6}
+                        sx={{ margin: "-12px" }}
+                        key={index}
+                        onBlur={() => setShowDescription()}
+                        onFocus={() => setShowDescription(index)}
+                        onMouseOver={() => setShowDescription(index)}
+                        onMouseOut={() => setShowDescription()}
+                      >
+                        <Card
+                          sx={{
+                            backgroundColor: "transparent",
+                            boxShadow: "none",
+                          }}
+                        >
+                          <CardMedia
+                            component="img"
+                            alt={data.title && data.title}
+                            image={data.icon && data.icon}
+                            title={data.title && data.title}
+                            sx={{
+                              width: "min(244px,100%)",
+                              height: "auto",
+                            }}
+                          />
+                        </Card>
+                      </Grid>
+                    ))}
+                  </Grid>
+                )}
+              {showDescription ? (
+                <Typography
+                  variant="body2"
+                  color="primary.main"
+                  textAlign="center"
+                >
+                  {ChainSectionConfigData.chains[showDescription].title}
+                </Typography>
+              ) : (
+                <Typography
+                  variant="body2"
+                  color="primary.main"
+                  textAlign="center"
+                >
+                  {ChainSectionConfigData.defaultChainDescription}
+                </Typography>
+              )}
+            </Stack>
           </Grid>
-        </Stack>
+
+          {/* Right Sub-section */}
+          <Grid item xs={12} sm={6}>
+            <Stack spacing={4}>
+              <Grid container alignItems="center">
+                {ChainSectionConfigData.titleIcon && (
+                  <Grid item xs={6}>
+                    <img
+                      src={ChainSectionConfigData.titleIcon}
+                      alt={
+                        ChainSectionConfigData.title &&
+                        ChainSectionConfigData.title
+                      }
+                      style={{ width: "min(220px, 100%)", height: "auto" }}
+                    />
+                  </Grid>
+                )}
+                {ChainSectionConfigData.title && (
+                  <Grid item xs={6}>
+                    <Typography
+                      variant={
+                        ChainSectionConfigData.titleVariant
+                          ? ChainSectionConfigData.titleVariant
+                          : "h1"
+                      }
+                    >
+                      {ChainSectionConfigData.title}
+                    </Typography>
+                  </Grid>
+                )}
+              </Grid>
+              {ChainSectionConfigData.description && (
+                <Typography
+                  component="p"
+                  variant="subtitle2"
+                  color="inherit"
+                  sx={
+                    ChainSectionConfigData.descriptionStyle &&
+                    ChainSectionConfigData.descriptionStyle
+                  }
+                >
+                  {ChainSectionConfigData.description}
+                </Typography>
+              )}
+              {ChainSectionConfigData.ctas &&
+                ChainSectionConfigData.ctas.length > 0 && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexWrap: "wrap",
+                      gap: 2,
+                      pt: 3,
+                      justifyContent: { xs: "center", sm: "flex-start" },
+                    }}
+                  >
+                    {ChainSectionConfigData.ctas.map((cta, index) => (
+                      <Button
+                        component="a"
+                        variant={cta.variant ? cta.variant : "contained"}
+                        key={index}
+                        size="large"
+                        href={cta.url && cta.url}
+                        target={cta.target && cta.target}
+                        sx={{
+                          borderRadius: 8,
+                          fontWeight: "700",
+                          // padding: "12px 34px",
+                          // textTransform: "capitalize",
+                        }}
+                        endIcon={cta.icon ? cta.icon : null}
+                      >
+                        {cta.title && cta.title}
+                      </Button>
+                    ))}
+                  </Box>
+                )}
+            </Stack>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
