7c1d524b2b2598aef20f4bb2d6fef8ddd11fa9d0
Kamlesh P <kamlesh@assetmantle.one>
2022-08-29 20:39:47 +0530

    refactored imports

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ExplanationSection from "../views/AboutUsExplanationSection";
-import VisionMissionSection from "../views/VisionMissionSection";
 import FAQSection from "../views/FAQSection";
+import VisionMissionSection from "../views/VisionMissionSection";
 
 export default function About() {
   return (
