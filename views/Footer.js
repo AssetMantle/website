60137c1f99a69a4b91c7d0b571fc947871f1f0bd
Jathin Kotian <jkmanipaladmission@gmail.com>
2022-09-13 18:50:43 +0530

    final commit

--- views/Footer.js
+++ views/Footer.js
@@ -2,10 +2,12 @@ import {
   Box,
   Button,
   Container,
+  Divider,
   Fab,
   Fade,
   Grid,
   Link,
+  Paper,
   Stack,
   Typography,
 } from "@mui/material";
@@ -14,14 +16,15 @@ import { FiLinkedin, FiGithub, FiTwitter } from "react-icons/fi";
 import { TbBrandTelegram, TbBrandDiscord } from "react-icons/tb";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import NorthEastIcon from "@mui/icons-material/NorthEast";
-
+import SocialIcons from "../components/SocialIcons";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PropTypes from "prop-types";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
+import NextLink from "../components/NextLink";
 
 const FooterConfigData = {
   showTopData: true,
-  titleVariant: "h5",
+  titleVariant: "body2",
   colOne: {
     title: "Email",
     list: [
@@ -38,64 +41,121 @@ const FooterConfigData = {
     ],
   },
   colTwo: {
-    image: "/images/Logo.svg",
-    title: "Assetmantle",
+    title: "Placeholder",
     list: [
       {
-        icon: <FiTwitter />,
-        href: "https://twitter.com/AssetMantle",
-      },
-      {
-        icon: <InstagramIcon />,
-        href: "https://www.instagram.com/assetmantle/",
-      },
-      {
-        icon: <FiGithub />,
-        href: "https://github.com/AssetMantle",
-      },
-      {
-        icon: <TbBrandTelegram />,
-        href: "https://t.me/assetmantlechat",
-      },
-      {
-        icon: <TbBrandDiscord />,
-        href: "https://discord.gg/BSdBQ4495d",
-      },
-    ],
-  },
-  colThree: {
-    title: "Quick Links",
-    list: [
-      {
-        title: "Want to know what we’re upto?",
+        title: "Documentation",
         link: {
-          icon: <NorthEastIcon />,
+          text: "Documentation",
+          href: "https://docs.assetmantle.one/",
+        },
+      },
+      {
+        title: "MediaKit",
+        link: {
+          text: "MediaKit",
+          href: "https://docs.assetmantle.one/MediaKit/",
+        },
+      },
+      {
+        title: "Blog",
+        link: {
           text: "Blog",
           href: "http://blog.assetmantle.one/",
         },
       },
       {
-        title: "Want to know more?",
+        title: "Whitepaper",
         link: {
-          icon: <NorthEastIcon />,
-          text: "Docs",
-          href: "https://docs.assetmantle.one/",
+          text: "Whitepaper",
+          href: "https://docs.assetmantle.one/AssetMantle_Whitepaper/",
         },
       },
       {
-        title: "Interested in transaction activities?",
+        title: "Buy $MNTL",
         link: {
-          icon: <NorthEastIcon />,
-          text: "Explorer",
+          text: "Buy $MNTL",
+          href: "https://app.osmosis.zone/?from=USDC&to=MNTL",
+        },
+      },
+    ],
+  },
+  colThree: {
+    title: "Products",
+    list: [
+      {
+        title: "MantlePlace",
+        link: {
+          text: "MantlePlace",
+          href: "https://marketplace.assetmantle.one/",
+        },
+      },
+      {
+        title: "MantleBuilder",
+        link: {
+          text: "MantleBuilder",
+          href: "https://docs.assetmantle.one/MantleBuilder_Overview/",
+        },
+      },
+      {
+        title: "MantleWallet",
+        link: {
+          text: "MantleWallet",
+          href: "https://wallet.assetmantle.one/",
+        },
+      },
+      {
+        title: "MantleExplorer",
+        link: {
+          text: "MantleExplorer",
           href: "https://explorer.assetmantle.one/",
         },
       },
       {
-        title: "want to view your portfolio & assets?",
+        title: "MantlePlace Devnet",
         link: {
-          icon: <NorthEastIcon />,
-          text: "Wallet",
-          href: "https://wallet.assetmantle.one/",
+          text: "MantlePlace Devnet",
+          href: "https://devnet.assetmantle.one/",
+        },
+      },
+    ],
+  },
+  colFour: {
+    title: "Placeholder",
+    list: [
+      {
+        title: "interNFT",
+        link: {
+          text: "interNFT",
+          href: "https://internft.org/",
+        },
+      },
+      {
+        title: "Grants",
+        link: {
+          text: "Grants",
+          href: "https://grants.assetmantle.one/",
+        },
+      },
+      {
+        title: "Airdrops",
+        link: {
+          text: "Airdrops",
+          href: "https://airdrop.assetmantle.one/",
+        },
+      },
+      {
+        title: "About",
+        link: {
+          text: "About",
+          href: "https://assetmantle.one/about",
+        },
+      },
+      {
+        title: "Ecosystems",
+        link: {
+          text: "Ecosystems",
+          href: "https://assetmantle.one/ecosystems",
         },
       },
     ],
@@ -164,177 +224,156 @@ ScrollTop.propTypes = {
 export default function Footer(props) {
   return (
     <>
-      <Box component="footer" sx={footerStyle}>
-        <Container
-          maxWidth="lg"
-          sx={{
-            borderTop: "1px solid",
-            borderColor: "primary.light",
-            pt: 5,
-          }}
-        >
-          {FooterConfigData.showTopData === true && (
-            <Grid container spacing={4} sx={{ pb: 3 }}>
-              <Grid item xs={12} sm={6} md={4}>
-                {FooterConfigData.colOne.title && (
-                  <Typography
-                    variant={
-                      FooterConfigData.titleVariant
-                        ? FooterConfigData.titleVariant
-                        : "h5"
-                    }
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {FooterConfigData.colOne.title}
-                  </Typography>
-                )}
-                {FooterConfigData.colOne.list &&
-                  Array.isArray(FooterConfigData.colOne.list) &&
-                  FooterConfigData.colOne.list.length > 0 && (
-                    <Stack sx={{ pt: 3 }} spacing={2}>
-                      {React.Children.toArray(
-                        FooterConfigData.colOne.list.map((item) => (
-                          <Stack spacing={2}>
-                            <Typography variant="h6">{item.title}</Typography>
-                            <Button
-                              component="a"
-                              variant="text"
-                              size="large"
-                              href={item.email && `mailto:${item.email}`}
-                              sx={{
-                                p: 0,
-                                justifyContent: {
-                                  xs: "center",
-                                  sm: "flex-start",
-                                },
-                                textTransform: "lowercase",
-                                // color: "primary.light",
-                                fontWeight: "400",
-                              }}
+      <Paper sx={{ py: 8 }} variant="translucent">
+        <Container maxWidth="lg">
+          <Stack
+            spacing={6}
+            // alignItems="stretch"
+            // justifyContent="space-between"
+          >
+            {FooterConfigData.showTopData === true && (
+              <Grid container spacing={4} backgroundColor="transparent">
+                <Grid item xs={12} sm={6} md={3}>
+                  {FooterConfigData.colOne.list &&
+                    Array.isArray(FooterConfigData.colOne.list) &&
+                    FooterConfigData.colOne.list.length > 0 && (
+                      <Stack
+                        sx={{ pt: 3 }}
+                        spacing={3}
+                        alignItems={{ xs: "center", md: "start" }}
+                      >
+                        {FooterConfigData.colOne.list.map((item, index) => (
+                          <Stack
+                            key={index}
+                            spacing={0.5}
+                            backgroundColor="transparent"
+                            alignItems={{ xs: "center", md: "start" }}
+                          >
+                            <Typography variant="caption">
+                              {item.title}
+                            </Typography>
+                            <NextLink
+                              color="primary.main"
+                              underline="hover"
+                              variant="caption"
                             >
                               {item.text}
-                            </Button>
+                            </NextLink>
                           </Stack>
-                        ))
-                      )}
-                    </Stack>
-                  )}
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Stack spacing={3}>
-                  {FooterConfigData.colTwo.image && (
-                    <img
-                      src={
-                        FooterConfigData.colTwo.image &&
-                        FooterConfigData.colTwo.image
-                      }
-                      alt={
-                        FooterConfigData.colTwo.title &&
-                        FooterConfigData.colTwo.title
-                      }
-                      style={{
-                        width: "min(164px,100%)",
-                        height: "auto",
-                        margin: "auto",
-                      }}
-                    />
-                  )}
+                        ))}
+                        <SocialIcons spacing={1} size="small" />
+                      </Stack>
+                    )}
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={3}
+                  sx={{
+                    textAlign: "center",
+                    // margin: { xs: 0, sm: "auto", md: 0 },
+                  }}
+                >
                   {FooterConfigData.colTwo.list &&
                     Array.isArray(FooterConfigData.colTwo.list) &&
                     FooterConfigData.colTwo.list.length > 0 && (
-                      <Stack
-                        spacing={3}
-                        direction="row"
-                        sx={{ justifyContent: "center" }}
-                      >
-                        {React.Children.toArray(
-                          FooterConfigData.colTwo.list.map((button) => (
-                            <>
-                              <Link
-                                sx={linkStyles}
-                                component="a"
-                                href={button.href && button.href}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                {button.icon && button.icon}
-                              </Link>
-                            </>
-                          ))
-                        )}
-                      </Stack>
-                    )}
-                </Stack>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                sx={{
-                  textAlign: "center",
-                  margin: { xs: 0, sm: "auto", md: 0 },
-                }}
-              >
-                {FooterConfigData.colThree.title && (
-                  <Typography
-                    variant={
-                      FooterConfigData.titleVariant
-                        ? FooterConfigData.titleVariant
-                        : "h5"
-                    }
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {FooterConfigData.colThree.title}
-                  </Typography>
-                )}
-                {FooterConfigData.colThree.list &&
-                  Array.isArray(FooterConfigData.colThree.list) &&
-                  FooterConfigData.colThree.list.length > 0 && (
-                    <Stack sx={{ pt: 3 }} spacing={2}>
-                      {React.Children.toArray(
-                        FooterConfigData.colThree.list.map((item) => (
-                          <Typography variant="caption">
-                            {item.title}
-                            <Button
-                              component="a"
-                              variant="text"
-                              size="large"
-                              href={item.link.href && item.link.href}
-                              sx={{
-                                p: 0,
-                                ml: 1,
-                                width: "max-content",
-                                textTransform: "Capitalize",
-                                fontWeight: "400",
-                              }}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              endIcon={item.link.icon && item.link.icon}
+                      <Stack sx={{ pt: 3 }} spacing={2}>
+                        {FooterConfigData.colTwo.list.map((item, index) => (
+                          <Typography
+                            variant="caption"
+                            key={`${index}sa${item}`}
+                          >
+                            <NextLink
+                              color="primary.main"
+                              underline="hover"
+                              variant="caption"
                             >
                               {item.link.text}
-                            </Button>
+                            </NextLink>
                           </Typography>
-                        ))
-                      )}
-                    </Stack>
-                  )}
+                        ))}
+                      </Stack>
+                    )}
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={3}
+                  sx={{
+                    textAlign: "center",
+                    // margin: { xs: 0, sm: "auto", md: 0 },
+                  }}
+                >
+                  {FooterConfigData.colThree.list &&
+                    Array.isArray(FooterConfigData.colThree.list) &&
+                    FooterConfigData.colThree.list.length > 0 && (
+                      <Stack sx={{ pt: 3 }} spacing={2}>
+                        {FooterConfigData.colThree.list.map((item, index) => (
+                          <Typography
+                            variant="caption"
+                            key={`${index}sa${item}`}
+                          >
+                            <NextLink
+                              color="primary.main"
+                              underline="hover"
+                              variant="caption"
+                            >
+                              {item.link.text}
+                            </NextLink>
+                          </Typography>
+                        ))}
+                      </Stack>
+                    )}
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={3}
+                  sx={{
+                    textAlign: "center",
+                    // margin: { xs: 0, sm: "auto", md: 0 },
+                  }}
+                >
+                  {FooterConfigData.colFour.list &&
+                    Array.isArray(FooterConfigData.colFour.list) &&
+                    FooterConfigData.colFour.list.length > 0 && (
+                      <Stack sx={{ pt: 3 }} spacing={2}>
+                        {FooterConfigData.colFour.list.map((item, index) => (
+                          <NextLink
+                            color="primary.main"
+                            underline="hover"
+                            variant="caption"
+                          >
+                            {item.link.text}
+                          </NextLink>
+                        ))}
+                      </Stack>
+                    )}
+                </Grid>
               </Grid>
-            </Grid>
-          )}
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ textAlign: "center" }}
-          >
-            © AssetMantle {new Date().getFullYear()} - All rights reserved{" "}
-          </Typography>
+            )}
+            <Divider sx={{ borderColor: "grey.600" }} />
+            <Stack
+              spacing={2}
+              direction={{ xs: "column", sm: "row" }}
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <img src="/static/Logo.svg" style={{ maxWidth: "200px" }}></img>
+              <Typography
+                variant="caption"
+                component="p"
+                sx={{ textAlign: "center" }}
+              >
+                © AssetMantle {new Date().getFullYear()} - All rights reserved{" "}
+              </Typography>
+            </Stack>
+          </Stack>
         </Container>
-      </Box>
+      </Paper>
       <ScrollTop {...props}>
         <Fab
           size="small"
