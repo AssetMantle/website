a6330e7116f7e92c11e0f9af6c1263c7834503ce
Jathin Kotian <jkmanipaladmission@gmail.com>
2022-09-13 12:16:50 +0530

    created a json object for accordion data

--- views/FAQSection.js
+++ views/FAQSection.js
@@ -1,15 +1,52 @@
+import { React, useState } from "react";
+import Section from "../components/Section";
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Grid,
   List,
   ListItem,
+  Link as MuiLink,
   Typography,
+  AccordionDetails,
+  AccordionSummary,
+  Accordion,
+  Grid,
 } from "@mui/material";
-import { React, useState } from "react";
+import Link from "next/link";
 import NextLink from "../components/NextLink";
-import Section from "../components/Section";
+
+const configData = {
+  list: [
+    {
+      title: "What is an NFT?",
+      info: "NFT stands for “Non-Fungible Token”. An NFT is an immutable token on the blockchain. It is cryptographically protected and can be treated as a digital certificate of ownership on the blockchain. NFTs on AssetMantle are part of the growing Cosmos ecosystem.",
+    },
+    {
+      title: "What is MantlePlace?",
+      info: "MantlePlace is the native NFT marketplace if AssetMantle.All creators on MantlePlace are verified for authenticity in an effort to minimize instances of frauds and rug pulls.",
+    },
+    {
+      title: "What is MantleBuilder?",
+      info: "MantleBuilder is the revolutionary no-code NFT marketplace builder from AssetMantle. It's simple drag-and-drop interface empowers creators and brands to build their robust and custom-branded NFT storefronts in the Cosmos ecosystem. Read more about MantleBuilder",
+    },
+    {
+      title: "What is IBC Protocol?",
+      info: "IBC stands for “Inter-Blockchain Communication”. It is an open-source protocol that allows sovereign blockchains in the Cosmos ecosystem to transfer assets and information among themselves. This empowers NFT creators and collectors to utilize   their NFTs across different chains and metaverses.",
+    },
+    {
+      title: "What makes AssetMantle special?",
+      info: "AssetMantle is built from the ground up to completely redefine digital asset ownership. Some noteworthy features include:",
+      accordionList: [
+        "Support for fractional NFTs",
+        "Negligible gas fees",
+        "Ledger hardware support",
+        "InterNFT standard",
+        "IPFS storage mechanism",
+        "Cross-chain interoperability",
+        "Support for multiple asset types",
+        "Real-world asset tokenization",
+      ],
+    },
+  ],
+};
 
 export default function FAQSection() {
   const [expanded, setExpanded] = useState(false);
@@ -22,158 +59,32 @@ export default function FAQSection() {
     <>
       <Section title="FAQ">
         <Grid container>
-          <Accordion
-            variant="translucent"
-            expanded={expanded === "panel1"}
-            onChange={handleChange("panel1")}
-          >
-            <AccordionSummary
-              aria-controls="panel1a-content"
-              id="panel1a-header"
+          {configData.list.map((ele, index) => (
+            <Accordion
+              variant="translucent"
+              expanded={expanded === `panel${index}`}
+              onChange={handleChange(`panel${index}`)}
             >
-              <Typography variant="body1">What is an NFT?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1">
-                NFT stands for “Non-Fungible Token”. An NFT is an immutable
-                token on the blockchain. It is cryptographically protected and
-                can be treated as a digital certificate of ownership on the
-                blockchain. NFTs on AssetMantle are part of the growing Cosmos
-                ecosystem.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            variant="translucent"
-            expanded={expanded === "panel2"}
-            onChange={handleChange("panel2")}
-          >
-            <AccordionSummary
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography variant="body1">What is MantlePlace?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1">
-                MantlePlace is the native NFT marketplace if AssetMantle.All
-                creators on MantlePlace are verified for authenticity in an
-                effort to minimize instances of frauds and rug pulls.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            variant="translucent"
-            expanded={expanded === "panel3"}
-            onChange={handleChange("panel3")}
-          >
-            <AccordionSummary
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography variant="body1">What is MantleBuilder?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1" sx={{ display: "inline" }}>
-                MantleBuilder is the revolutionary no-code NFT marketplace
-                builder from AssetMantle. It's simple drag-and-drop interface
-                empowers creators and brands to build their robust and
-                custom-branded NFT storefronts in the Cosmos ecosystem. Read
-                more about MantleBuilder{" "}
-              </Typography>
-              <NextLink
-                href="https://docs.assetmantle.one/MantleBuilder_Overview"
-                target="_blank"
+              <AccordionSummary
+                aria-controls={`panel${index}a-content`}
+                id={`panel${index}a-header`}
               >
-                here.
-              </NextLink>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            variant="translucent"
-            expanded={expanded === "panel4"}
-            onChange={handleChange("panel4")}
-          >
-            <AccordionSummary
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography variant="body1">What is IBC Protocol?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1">
-                IBC stands for “Inter-Blockchain Communication”. It is an
-                open-source protocol that allows sovereign blockchains in the
-                Cosmos ecosystem to transfer assets and information among
-                themselves. This empowers NFT creators and collectors to utilize
-                their NFTs across different chains and metaverses.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            variant="translucent"
-            expanded={expanded === "panel5"}
-            onChange={handleChange("panel5")}
-          >
-            <AccordionSummary
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography variant="body1">
-                What makes AssetMantle special?
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="body1">
-                AssetMantle is built from the ground up to completely redefine
-                digital asset ownership. Some noteworthy features include:{" "}
-              </Typography>
-              <List>
-                <ListItem>
-                  <Typography variant="body1">Open-source modules</Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">IBC protocol enabled</Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">
-                    Support for fractional NFTs
-                  </Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">Negligible gas fees</Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">
-                    Ledger hardware support
-                  </Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">InterNFT standard</Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">
-                    IPFS storage mechanism
-                  </Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">
-                    Cross-chain interoperability
-                  </Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">
-                    Support for multiple asset types
-                  </Typography>
-                </ListItem>{" "}
-                <ListItem>
-                  <Typography variant="body1">
-                    Real-world asset tokenization
-                  </Typography>
-                </ListItem>
-              </List>
-            </AccordionDetails>
-          </Accordion>
+                <Typography variant="body1">{ele.title}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">{ele.info}</Typography>
+                {"accordionList" in ele && (
+                  <List>
+                    {ele.accordionList.map((item) => (
+                      <ListItem>
+                        <Typography variant="body1">{item}</Typography>
+                      </ListItem>
+                    ))}
+                  </List>
+                )}
+              </AccordionDetails>
+            </Accordion>
+          ))}
         </Grid>
       </Section>
     </>
