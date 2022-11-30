cbbbfbc757e1a0cb5be69df87a83d9a79893dfe1
Jathin Kotian <jathinkotian@Jathins-MacBook-Air.local>
2022-11-30 12:31:37 +0530

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,30 @@ import PropTypes from "prop-types";
 import * as React from "react";
 import Layout from "../components/layout";
 import theme from "../config/theme";
+import { useEffect } from "react";
+import * as ga from "../lib";
+import { useRouter } from "next/router";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
