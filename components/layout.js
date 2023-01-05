0200496a0e67cd66e312c0dec7cc6d6ddaf91925
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-05 20:53:55 +0600

    🛠️💅removed christmas

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,9 @@
 import Footer from "../views/Footer";
 import Header from "../views/Header";
-import Christmas from "./Christmas";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Christmas /> {/* to remove christmas styles remove this component */}
       <Header />
       <main>{children}</main>
       <Footer />
