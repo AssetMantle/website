cb1f86ce3ef00caee2be425535474b2f92fd35e1
Jathin Kotian <jathin@assetmantle.one>
2023-01-17 15:54:37 +0530

    added mantleplace banner

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,22 @@
 import Footer from "../views/Footer";
 import Header from "../views/Header";
+import { Alert } from "@mui/material";
+import { useState } from "react";
 
 export default function Layout({ children }) {
+  const [showAlert, setShowAlert] = useState(true);
+
   return (
     <>
+      {showAlert ? (
+        <Alert
+          icon={false}
+          onClose={() => {
+            setShowAlert(false);
+          }}
+          sx={{ backgroundColor: "primary.main" }}
+        />
+      ) : null}
       <Header />
       <main>{children}</main>
       <Footer />
