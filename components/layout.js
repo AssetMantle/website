7243daffca7cacb07b6729e74d51181a2e41605b
Jathin Kotian <jathin@assetmantle.one>
2023-01-17 18:34:41 +0530

    added button to banner with text;

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,8 @@
 import Footer from "../views/Footer";
 import Header from "../views/Header";
-import { Alert } from "@mui/material";
+import { Alert, Typography, Link, Box, Button } from "@mui/material";
 import { useState } from "react";
+import CelebrationIcon from "@mui/icons-material/Celebration";
 
 export default function Layout({ children }) {
   const [showAlert, setShowAlert] = useState(true);
@@ -14,9 +15,36 @@ export default function Layout({ children }) {
           onClose={() => {
             setShowAlert(false);
           }}
-          sx={{ backgroundColor: "primary.main", color: "navBg" }}
+          sx={{
+            backgroundColor: "primary.main",
+            color: "navBg",
+          }}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, vel.
+          {" "}
+          <Button
+            component={Link}
+            style={{ margin: 4 }}
+            to="https://marketplace.assetmantle.one/"
+            variant="contained"
+            color="primary"
+          >
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              <CelebrationIcon />
+              <Typography px={2} variant="body1">
+                Click here for limited early access to MantlePlace NFTs
+              </Typography>
+              <CelebrationIcon />
+            </Box>
+          </Button>
         </Alert>
       ) : null}
       <Header />
