8ccc255e25ef70c0c62437b5fe063654403398b0
Kamlesh P <kamlesh@assetmantle.one>
2023-01-19 19:57:51 +0530

    minor changes to banner

--- components/layout.js
+++ components/layout.js
@@ -1,56 +1,14 @@
+import { useState } from "react";
 import Footer from "../views/Footer";
 import Header from "../views/Header";
-import { Alert, Typography, Link, Box, Button } from "@mui/material";
-import { useState } from "react";
-import CelebrationIcon from "@mui/icons-material/Celebration";
+import Banner from "./Banner";
 
 export default function Layout({ children }) {
   const [showAlert, setShowAlert] = useState(true);
 
   return (
     <>
-      {showAlert ? (
-        <Alert
-          icon={false}
-          onClose={() => {
-            setShowAlert(false);
-          }}
-          sx={{
-            backgroundColor: "primary.main",
-            color: "navBg",
-            borderRadius: 0,
-            py: 0,
-            "& > *:first-child": {
-              flex: "1",
-              py: 0,
-            },
-          }}
-        >
-          {" "}
-          <Link
-            component={Link}
-            display="flex"
-            style={{ margin: "auto", width: "100%" }}
-            href="https://marketplace.assetmantle.one/"
-            target="_blank"
-            variant="text"
-            color="primary"
-            py={1}
-          >
-            <Typography
-              variant="caption"
-              color="navBg"
-              textTransform="capitalize"
-              textAlign="center"
-              style={{ fontWeight: "400" }}
-              mx="auto"
-            >
-              🎉 Click here for limited early access to{" "}
-              <strong>MantlePlace NFTs</strong> 🎉
-            </Typography>
-          </Link>
-        </Alert>
-      ) : null}
+      <Banner />
       <Header />
       <main>{children}</main>
       <Footer />
