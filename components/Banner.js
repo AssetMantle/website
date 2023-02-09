ff01f24e7f221cffaacb065adf1b568eb856c324
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-09 20:53:43 +0600

    🛠️banner created with carousel

--- components/Banner.js
+++ components/Banner.js
@@ -1,5 +1,8 @@
-import { Alert, Link, Typography } from "@mui/material";
+import { Alert, Box, Button, Link, Typography } from "@mui/material";
 import React, { useState } from "react";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 
 export default function Banner() {
   const [showAlert, setShowAlert] = useState(true);
@@ -23,39 +26,145 @@ export default function Banner() {
     </Typography>
   );
 
-  const alertJSX = showAlert ? (
-    <Alert
-      icon={false}
-      onClose={() => {
-        setShowAlert(false);
-      }}
+  const alertJSX = (
+    <Box
       sx={{
-        backgroundColor: "primary.main",
-        color: "navBg",
-        borderRadius: 0,
+        width: "100%",
+        position: "relative",
         display: "flex",
-        "& > *:first-child": {
-          flex: "1",
-          py: 0,
-        },
+        justifyContent: "center",
       }}
     >
-      {" "}
-      <Link
-        component={Link}
-        display="flex"
-        style={{ margin: "auto", width: "100%" }}
-        href={url}
-        target="_blank"
-        variant="text"
-        color="primary"
-        py={1}
-        alignItems="center"
+      <Splide
+        hasTrack={false}
+        options={{
+          type: "loop",
+          rewind: true,
+          width: "100%",
+          gap: "32px",
+          perPage: 1,
+          pagination: false,
+        }}
       >
-        {bannerMessageJSX}
-      </Link>
-    </Alert>
-  ) : null;
+        <SplideTrack>
+          <SplideSlide>
+            <Alert
+              icon={false}
+              severity="info"
+              sx={{
+                backgroundColor: "primary.main",
+                color: "navBg",
+                borderRadius: 0,
+                display: "flex",
+                "& > *:first-of-type": {
+                  flex: "1",
+                  py: 0,
+                },
+              }}
+            >
+              {" "}
+              <Link
+                component={Link}
+                display="flex"
+                style={{ margin: "auto", width: "100%" }}
+                href={url}
+                target="_blank"
+                py={1}
+                alignItems="center"
+              >
+                {bannerMessageJSX}
+              </Link>
+            </Alert>
+          </SplideSlide>
+          <SplideSlide>
+            <Alert
+              icon={false}
+              severity="info"
+              sx={{
+                backgroundColor: "primary.main",
+                color: "navBg",
+                borderRadius: 0,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                py: 0,
+              }}
+            >
+              <Typography
+                variant="caption"
+                textTransform="capitalize"
+                textAlign="center"
+                style={{ fontWeight: "400" }}
+                mx="auto"
+              >
+                Participate in the Bug Bounty Program{" "}
+                <Link
+                  component={Link}
+                  href="https://github.com/AssetMantle/Bug-Bounty"
+                  target="_blank"
+                  variant="text"
+                  color="background.default"
+                  underline="none"
+                  fontWeight="500"
+                >
+                  here
+                </Link>{" "}
+                and win prizes!
+              </Typography>
+            </Alert>
+          </SplideSlide>
+        </SplideTrack>
+        <Box className="splide__arrows">
+          <Button
+            className="splide__arrow splide__arrow--prev"
+            sx={{
+              background: "transparent !important",
+              outline: "none !important",
+              position: "absolute",
+              left: "10px !important",
+              minWidth: "max-content",
+              transform: {
+                xs: "translateX(0%) translateY(-50%) !important",
+                md: "translateX(0%) translateY(-50%) !important",
+              },
+              "&.splide__arrow svg": {
+                fill: "navBg",
+              },
+            }}
+          >
+            <EastOutlinedIcon
+              sx={{
+                fontSize: "20px",
+              }}
+            />
+          </Button>
+          <Button
+            className="splide__arrow splide__arrow--next"
+            sx={{
+              background: "transparent !important",
+              outline: "none !important",
+              position: "absolute",
+              right: "10px !important",
+              minWidth: "max-content",
+              transform: {
+                xs: "translateX(0%) translateY(-50%) !important",
+                md: "translateX(0%) translateY(-50%) !important",
+              },
+              "&.splide__arrow svg": {
+                fill: "navBg",
+              },
+            }}
+          >
+            <EastOutlinedIcon
+              sx={{
+                fontSize: "20px",
+              }}
+            />
+          </Button>
+        </Box>
+      </Splide>
+    </Box>
+  );
 
   return <>{alertJSX}</>;
 }
