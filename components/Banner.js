4de6337e9f5d27c42518320fdf34c999d51e9e69
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-09 21:05:15 +0600

    🛠️💅🪲fixes

--- components/Banner.js
+++ components/Banner.js
@@ -18,7 +18,7 @@ export default function Banner() {
       color="navBg"
       textTransform="capitalize"
       textAlign="center"
-      style={{ fontWeight: "400" }}
+      style={{ fontWeight: "400", width: "80%" }}
       mx="auto"
     >
       {bannerIcon}&nbsp;&nbsp;Click here for limited early access to{" "}
@@ -87,30 +87,35 @@ export default function Banner() {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
-                py: 0,
+                "& > *:first-of-type": {
+                  flex: "1",
+                  py: 0,
+                },
               }}
             >
-              <Typography
-                variant="caption"
-                textTransform="capitalize"
-                textAlign="center"
-                style={{ fontWeight: "400" }}
-                mx="auto"
+              <Link
+                component={Link}
+                href="https://github.com/AssetMantle/Bug-Bounty"
+                target="_blank"
+                variant="text"
+                color="background.default"
+                underline="none"
+                fontWeight="500"
+                style={{ margin: "auto", width: "100%" }}
               >
-                Participate in the Bug Bounty Program{" "}
-                <Link
-                  component={Link}
-                  href="https://github.com/AssetMantle/Bug-Bounty"
-                  target="_blank"
-                  variant="text"
-                  color="background.default"
-                  underline="none"
-                  fontWeight="500"
+                <Typography
+                  component="p"
+                  variant="caption"
+                  textTransform="capitalize"
+                  textAlign="center"
+                  style={{ fontWeight: "400", width: "80%" }}
+                  mx="auto"
+                  py={1}
                 >
-                  here
-                </Link>{" "}
-                and win prizes!
-              </Typography>
+                  Participate in the Bug Bounty Program <strong>here</strong>{" "}
+                  and win prizes!
+                </Typography>
+              </Link>
             </Alert>
           </SplideSlide>
         </SplideTrack>
