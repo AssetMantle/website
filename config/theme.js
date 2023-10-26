9e941eaee4c2af74d4962d3102d3eceace1c5610
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-10-26 15:29:49 +0600

    📕updated

--- config/theme.js
+++ config/theme.js
@@ -32,7 +32,7 @@ const baseTheme = createTheme({
     h2: {
       fontSize: "3.75rem",
       fontWeight: 800,
-      letterSpacing: "-4%",
+      letterSpacing: "-2.4px",
       textTransform: "capitalize",
       lineHeight: 1,
     },
@@ -47,28 +47,28 @@ const baseTheme = createTheme({
       fontSize: "2rem",
       lineHeight: 1.31,
       fontWeight: 700,
-      letterSpacing: "-2.1%",
+      letterSpacing: "-0.672px",
       textTransform: "capitalize",
     },
     h5: {
       fontSize: "1.5rem",
       lineHeight: 1.31,
       fontWeight: 700,
-      letterSpacing: "-2.1%",
+      letterSpacing: "-0.456px",
       textTransform: "capitalize",
     },
     h6: {
       fontSize: "1.125rem",
       lineHeight: 1.31,
       fontWeight: 700,
-      letterSpacing: "-2.1%",
+      letterSpacing: "-0.252px",
       textTransform: "capitalize",
     },
     subtitle1: {
       fontSize: "1.75rem",
       fontWeight: 200,
       lineHeight: 1.42,
-      letterSpacing: "-2%",
+      letterSpacing: "-0.56px",
     },
     subtitle2: {
       fontSize: "1.375rem",
@@ -79,14 +79,13 @@ const baseTheme = createTheme({
     body1: {
       fontSize: "1.125rem",
       fontWeight: 600,
-      lineHeight: 1,
-      letterSpacing: "-1.4%",
+      lineHeight: 1.38,
+      letterSpacing: "-0.252px",
     },
     body2: {
       fontSize: "1rem",
       fontWeight: 400,
       lineHeight: 1.375,
-      letterSpacing: "-1.1%",
     },
     caption: {
       fontSize: "0.75rem",
@@ -95,7 +94,7 @@ const baseTheme = createTheme({
     },
     button: {
       fontSize: "1.25rem !important",
-      lineHeight: 1.286,
+      lineHeight: 1,
       letterSpacing: "-1.7%",
       fontWeight: 600,
     },
