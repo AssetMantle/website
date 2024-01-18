639052590bd25f32b8c5e643600d2aef45c0383b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-01-18 12:38:08 +0600

    💅🛠️background added

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Footer from "../views/Footer";
 import Header from "../views/Header";
-import Banner from "./Banner";
+// import Banner from "./Banner";
+// import Christmas from "./Christmas";
+import Background from "./Background";
 
 export default function Layout({ children }) {
   const [showAlert, setShowAlert] = useState(true);
@@ -9,6 +11,7 @@ export default function Layout({ children }) {
   return (
     <>
       {/* <Banner /> */}
+      <Background />
       <Header />
       <main>{children}</main>
       <Footer />
