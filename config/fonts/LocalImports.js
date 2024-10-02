783804014c204b74d5d274827d36060dc88cd3bd
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-10-02 12:21:35 +0600

    🛠️💅📕🏷️initial setups

---
+++ config/fonts/LocalImports.js
@@ -0,0 +1,16 @@
+import localFont from "next/font/local";
+
+export const PixeloidMono = localFont({
+  src: "./PixeloidMono.woff2",
+  display: "swap",
+});
+
+export const PixeloidSans = localFont({
+  src: "./PixeloidSans.woff2",
+  display: "swap",
+});
+
+export const PixeloidSansBold = localFont({
+  src: "./PixeloidSansBold.woff2",
+  display: "swap",
+});
--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
-};
