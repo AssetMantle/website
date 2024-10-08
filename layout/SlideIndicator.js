2e2aeb802633ca824d2a5e4dcafb205819e8971e
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-10-08 16:24:03 +0600

    🛠️📕💅🏷️Section indicator added

---
+++ layout/SlideIndicator.js
@@ -0,0 +1,46 @@
+import { BPdotsSquare } from "@/config/fonts/LocalImports";
+import React from "react";
+
+export default function SlideIndicator({ indicator }) {
+  const sectionMap = [
+    {
+      title: "metrics",
+    },
+    {
+      title: "business",
+    },
+    {
+      title: "builder",
+    },
+    {
+      title: "user",
+    },
+    {
+      title: "Ecosystem",
+    },
+    // {
+    //   title: "metrics",
+    // },
+  ];
+
+  return (
+    <div className="am-slideIndicator">
+      {React.Children.toArray(
+        sectionMap.map((el, ind) => (
+          <button
+            className={`indicator ${BPdotsSquare.className} mw-${indicator}${
+              indicator === ind + 1 ? " active" : ""
+            }`}
+            onClick={() =>
+              window &&
+              window.innerHeight &&
+              window.scrollTo(0, window.innerHeight * (ind + 1))
+            }
+          >
+            {el?.title}
+          </button>
+        ))
+      )}
+    </div>
+  );
+}
