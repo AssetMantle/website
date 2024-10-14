13e5da5f07339adc12723787bfe614eeedd299fb
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-10-14 21:20:37 +0600

    🛠️📕💅🏷️Metrics Section added

---
+++ view/Metrics.js
@@ -0,0 +1,50 @@
+import MetricsGame from "@/components/games/MetricsGame";
+import { BPdotsSquare } from "@/config/fonts/LocalImports";
+import React from "react";
+
+export default function Metrics({ indicator }) {
+  const Stats = [
+    {
+      title: "Transactions",
+      number: "9,655,088",
+    },
+    {
+      title: "Accounts",
+      number: "101,190",
+    },
+    {
+      title: "Identities Named",
+      number: "42,870",
+    },
+    {
+      title: "Asset Classes",
+      number: "1",
+    },
+  ];
+
+  return (
+    <section className="am-metrics-parent" id="metrics">
+      {indicator === 1 && <MetricsGame />}
+      <div className={`am-container am-metrics ${BPdotsSquare.className}`}>
+        <div className="content">
+          <h2>metrics</h2>
+          <div className="list">
+            {React.Children.toArray(
+              Stats?.map((stat) => (
+                <div className="option">
+                  <div className="dot">
+                    <span></span>
+                  </div>
+                  <div className="content">
+                    <h3>{stat?.title}</h3>
+                    <p>{stat?.number}</p>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
