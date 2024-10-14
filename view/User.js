2c4c2a0ee690d2cb12b403a5d542c214c9f1bc7a
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-10-14 21:21:25 +0600

    🛠️📕💅🏷️User Section added

---
+++ view/User.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { CgArrowTopRightR } from "react-icons/cg";
+import { BPdotsSquare, DMSans } from "@/config/fonts/LocalImports";
+
+export default function User() {
+  const DATA = [
+    {
+      title: "MantleWallet",
+      title_icon: <CgArrowTopRightR />,
+      href: "https://wallet.assetmantle.one/",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at lacinia tortor. ",
+    },
+    {
+      title: "MantlePlace",
+      title_icon: <CgArrowTopRightR />,
+      href: "https://marketplace.assetmantle.one/",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at lacinia tortor. ",
+    },
+    {
+      title: "Ami Names",
+      title_icon: <CgArrowTopRightR />,
+      href: "https://names.assetmantle.one/",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at lacinia tortor. ",
+    },
+    {
+      title: "Others",
+      title_icon: <CgArrowTopRightR />,
+      href: "",
+    },
+  ];
+
+  return (
+    <section className="am-user-parent" id="user">
+      <div className={`am-container am-user ${BPdotsSquare.className}`}>
+        <div className="content">
+          <h2>user</h2>
+          <div className="list">
+            {React.Children.toArray(
+              DATA?.map((item) => (
+                <div className="option">
+                  <div className="dot">
+                    <span></span>
+                  </div>
+                  <div className="content">
+                    <h3>
+                      <a
+                        href={item?.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {item?.title}
+                        {item?.title_icon}
+                      </a>
+                    </h3>
+                    {item?.details && (
+                      <p className={`${DMSans.className}`}>{item?.details}</p>
+                    )}
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
