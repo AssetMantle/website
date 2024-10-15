51ab9ae4b3261f8a0bda66ba4d83fb44e3b3231b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-10-15 18:19:57 +0600

    🛠️Section games removed

--- view/Hero.js
+++ view/Hero.js
@@ -1,4 +1,3 @@
-import HeroGame from "@/components/games/HeroGame";
 import { BPdotsSquare } from "@/config/fonts/LocalImports";
 import React from "react";
 
@@ -6,7 +5,6 @@ export default function Hero({ indicator }) {
   return (
     <section id="hero" className="am-hero-bg">
       <div className="timeline"></div>
-      {indicator === 0 && <HeroGame />}
       <div className="am-container am-hero">
         <h1 className={`${BPdotsSquare.className}`}>Lorem IPsum Dolor</h1>
       </div>
