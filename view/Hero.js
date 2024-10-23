d4ee31c78eafd8fc22217e1bd5de39ca2e65031a
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-10-23 07:13:10 +0600

    🦄logo change & 🛠️show after the pattern loads added

--- view/Hero.js
+++ view/Hero.js
@@ -1,12 +1,22 @@
 import { BPdotsSquare } from "@/config/fonts/LocalImports";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Hero() {
+export default function Hero({ indicator }) {
+  const [Show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (indicator === 0) {
+      const timeout = setTimeout(() => setShow(true), 6500);
+
+      return () => clearTimeout(timeout);
+    } else setShow(false);
+  }, [indicator]);
+
   return (
     <section id="hero" className="am-hero-bg">
       <div className="timeline"></div>
       <div className="am-container am-hero">
-        <h1 className={`${BPdotsSquare.className}`}>
+        <h1 className={`${BPdotsSquare.className}${Show ? " active" : ""}`}>
           Assetization Infrastructure Layer
         </h1>
       </div>
